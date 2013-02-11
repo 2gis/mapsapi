c24a80b36447f99293a4e72ac7fdcd3d801afc0f
AndreyChizh <info.andreychizh@gmail.com>
2013-02-11 17:39:21 +0200

    JSAPI-2541 : Add files

--- sandbox/AndreyChizh/ajax/ajax.js
+++ sandbox/AndreyChizh/ajax/ajax.js
@@ -6,6 +6,10 @@
  */
 L.ajax = function(params) {
     var query = '',
+        head,
+        script,
+        callbackId,
+        callbackName,
         resUrl;
 
     var url = params.url,
@@ -15,41 +19,50 @@ L.ajax = function(params) {
         error = params.error || function() {},
         complete = params.complete || function() {};
 
+    head = document.getElementsByTagName('head')[0];
 
-    var head = document.getElementsByTagName('head')[0];
-    var script = L.DomUtil.create('script', '', head);
-    script.type = 'text/javascript';
-    script.async = true;
+    callbackId = 'dg_' + ('' + Math.random()).slice(2);
+    callbackName = 'L.ajax.callback.' + callbackId;
 
-    var callbackId = "dg_" + ("" + Math.random()).slice(2);
-    var callbackName = "L.ajax.callback." + callbackId;
-
-
-    if (callbackId) {
-        L.ajax.callback[callbackId] = function(data) {
-            head.removeChild(script);
-            delete L.ajax.callback[callbackId];
-            success(data);
-        };
-    }
-
-    for (var param in data) {
-        if (data.hasOwnProperty(param)) {
-            query += encodeURIComponent(param) + '=' + encodeURIComponent(data[param]) + '&';
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            query += encodeURIComponent(key) + '=' + encodeURIComponent(data[key]) + '&';
         }
     }
 
-    if (url.indexOf("?") === -1) {
-        resUrl = url + "?" + query + 'callback=' + callbackName;
+    if (url.indexOf('?') === -1) {
+        resUrl = url + '?' + query + 'callback=' + callbackName;
     } else {
-        resUrl = url + "&" + query + 'callback=' + callbackName;
+        resUrl = url + '&' + query + 'callback=' + callbackName;
+    }
+
+    try {
+        L.ajax.callback[callbackId] = function(data) {
+            success(data);
+            head.removeChild(script);
+            delete L.ajax.callback[callbackId];
+        };
+    } catch (e) {
+        error({ url: resUrl, event: e });
     }
 
     beforeSend();
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
     script.src = resUrl;
+    head.appendChild(script);
     complete();
 
+    script.onerror = function(e) {
+        error({ url: resUrl, event: e });
+    };
+
     return callbackId;
+
+    var test = function(cid) {
+        console.log(cid)
+    }
 
 };
 
