1e7917440d2f7ffd2a7d99968af36bf65f4864d7
AndreyChizh <info.andreychizh@gmail.com>
2013-02-12 14:24:33 +0200

    JSAPI-2541 : AJAX

--- L.DGAjax/test/L.DGAjaxSpec.js
+++ L.DGAjax/test/L.DGAjaxSpec.js
@@ -1,6 +1,6 @@
 describe("DG AJAX", function() {
 
-    it("should be return callback ID", function() {
+    it("should be return cancel callback function", function() {
 
 
         expect(1).toEqual(1);
