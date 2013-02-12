6ee1493202178de5441ac6db77aa916bc5ac987c
Dima Rudenko <dm.rudenko@2gis.kiev.ua>
2013-02-12 17:08:25 +0200

    JSAPI-2547: ProjectDetector

--- sandbox/DimaRudenko/ProjectDetector/ProjectDetector.js
+++ sandbox/DimaRudenko/ProjectDetector/ProjectDetector.js
@@ -3,8 +3,9 @@
  * Version 0.0.1
  * Dima Rudenko
  */
+
 L.Map.mergeOptions({
-    projectDetector: true
+    projectDetector:true
 });
 
 L.ProjectDetector = L.Handler.extend({
@@ -28,7 +29,7 @@ L.ProjectDetector = L.Handler.extend({
             .on('move', this._projectChange, this);
     },
 
-    removeHooks: function () {
+    removeHooks:function () {
         this._map
             .off('move', this._projectChange, this);
     },
@@ -40,83 +41,17 @@ L.ProjectDetector = L.Handler.extend({
 
     _getProjectList:function () {
         var options = this.options;
-        JSONP.get(
-            this.options.url,
-            {
+        L.ajax({
+            url:options.url,
+            data:{
                 key:options.key,
                 version:options.version,
-                output:options.output
+                output:options.output,
+                lang:options.lang
             },
-            options.callback
-        );
+            success:options.callback
+        });
     }
 });
+
 L.Map.addInitHook('addHandler', 'projectDetector', L.ProjectDetector);
-
-
-/*
- * Usage:
- *
- * JSONP.get( 'someUrl.php', {param1:'123', param2:'456'}, function(data){
- *   //do something with data, which is the JSON object you should retrieve from someUrl.php
- * });
- */
-var JSONP = (function () {
-    var counter = 0, head, query, key, window = this, config = {};
-
-    function load(url) {
-        var script = document.createElement('script'),
-            done = false;
-        script.src = url;
-        script.async = true;
-
-        script.onload = script.onreadystatechange = function () {
-            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
-                done = true;
-                script.onload = script.onreadystatechange = null;
-                if (script && script.parentNode) {
-                    script.parentNode.removeChild(script);
-                }
-            }
-        };
-        if (!head) {
-            head = document.getElementsByTagName('head')[0];
-        }
-        head.appendChild(script);
-    }
-
-    function encode(str) {
-        return encodeURIComponent(str);
-    }
-
-    function jsonp(url, params, callback, callbackName) {
-        query = (url || '').indexOf('?') === -1 ? '?' : '&';
-        params = params || {};
-        for (key in params) {
-            if (params.hasOwnProperty(key)) {
-                query += encode(key) + "=" + encode(params[key]) + "&";
-            }
-        }
-        var jsonp = "json" + (++counter);
-        window[ jsonp ] = function (data) {
-            callback(data);
-            try {
-                delete window[ jsonp ];
-            } catch (e) {
-            }
-            window[ jsonp ] = null;
-        };
-
-        load(url + query + (callbackName || config['callbackName'] || 'callback') + '=' + jsonp);
-        return jsonp;
-    }
-
-    function setDefaults(obj) {
-        config = obj;
-    }
-
-    return {
-        get:jsonp,
-        init:setDefaults
-    };
-}());
