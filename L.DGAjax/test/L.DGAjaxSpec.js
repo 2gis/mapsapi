b8d964b1e36e76dcb81b221e3072f3edb67648eb
AndreyChizh <info.andreychizh@gmail.com>
2013-02-13 17:23:06 +0200

    JSAPI-2541 : AJAX

--- L.DGAjax/test/L.DGAjaxSpec.js
+++ L.DGAjax/test/L.DGAjaxSpec.js
@@ -1,86 +1,86 @@
-describe("DG AJAX", function() {
+describe("DG AJAX Module", function() {
 
     it("should be return cancel callback function", function() {
 
         var ajax = L.DGAjax({
-            url: 'http://127.0.0.1:3000/test',
-            data: {
-                output: 'jsonp',
-                key: 'ruxlih0718',
-                version: 1.3,
-                lang: 'ru'
-            }
+            url: 'http://127.0.0.1:3000/test'
         });
 
         expect(ajax).toBeDefined();
         expect(ajax.cancel).toBeDefined();
     });
 
-    it("should be get data", function() {
+    it("should get a success responce", function () {
+        var callback = jasmine.createSpy();
 
-        var ajax = L.DGAjax({
+        L.DGAjax({
             url: 'http://127.0.0.1:3000/test',
-            success: function(data) {
-                console.log(data);
-            },
-            error: function(error) {
-                console.log(error);
-            }
+            success: callback
         });
 
+        waitsFor(function() {
+            return callback.callCount > 0;
+        });
 
+        runs(function() {
+            expect(callback).toHaveBeenCalled();
+            expect(callback).toHaveBeenCalledWith({ test : 'ok' });
+        });
 
-
-        //expect(ajax).toBeDefined();
-        //expect(ajax.cancel).toBeDefined();
     });
 
+    it("should get a error responce", function () {
+        var callback = jasmine.createSpy();
 
+        L.DGAjax({
+            url: 'http://127.0.0.1:3000/cat',
+            error: callback
+        });
 
+        waitsFor(function() {
+            return callback.callCount > 0;
+        });
 
+        runs(function() {
+            expect(callback).toHaveBeenCalled();
+        });
 
+    });
 
+    it("should calls a beforeSend callback", function () {
+        var callback = jasmine.createSpy();
 
+        L.DGAjax({
+            url: 'http://127.0.0.1:3000/test',
+            beforeSend: callback
+        });
 
-//    it("should be return cancel callback function", function() {
-//
-//// before
-//
-//        var scriptMock = {};
-//
-//
-//        L.DGAjax._createScript = function() {
-//            return scriptMock;
-//        };
-//
-//
-////  test1
-//        var spyFunc = function() {
-//
-//        }
-//
-//        var ajax = L.DGAjax({
-//            url: 'http://catalog.api.2gis.ru/project/list',
-//            data: {
-//                output: 'jsonp',
-//                key: 'ruxlih0718',
-//                version: 1.3,
-//                lang: 'ru'
-//            },
-//            success : spyFunc
-//        });
-//
-//        myData = {}
-//        scriptMock._cbk(myData);
-//
-//        spyFunc.shouldBeCalled().shoudGetArgument(myData);
-//
-//
-//
-//
-//
-//    });
+        waitsFor(function() {
+            return callback.callCount > 0;
+        });
 
+        runs(function() {
+            expect(callback).toHaveBeenCalled();
+        });
 
+    });
 
+    it("should calls a complete callback", function () {
+        var callback = jasmine.createSpy();
+
+        L.DGAjax({
+            url: 'http://127.0.0.1:3000/test',
+            complete: callback
+        });
+
+        waitsFor(function() {
+            return callback.callCount > 0;
+        });
+
+        runs(function() {
+            expect(callback).toHaveBeenCalled();
+        });
+
+    });
+    
 });
--- L.DGAjax/test/server.js
+++ L.DGAjax/test/server.js
@@ -7,8 +7,12 @@ http.createServer(function(request, response) {
         var data = requestParam.query.callback + '({"test":"ok"})';
         response.writeHead(200, {"Content-Type": "application/javascript"});
         response.end(data);
-        console.log('Test server send response: ' + data);
+    } else {
+        var data = '<html><body><h1>404 Not Found</h1></body></html>';
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.end(data);
     }
+    console.log('Test server send response: ' + data);
 }).listen(3000);
 
 console.log('Test server running on port 3000');
