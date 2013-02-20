fd722cf066d1313227505a7488b126460d894642
Dima Rudenko <dm.rudenko@2gis.kiev.ua>
2013-02-20 18:19:36 +0200

    JSAPI-2547: write tests

--- L.DGProjectDetector/test/DGProjectDetectorSpec.js
+++ L.DGProjectDetector/test/DGProjectDetectorSpec.js
@@ -24,10 +24,93 @@ describe("DG Project Detector Module", function () {
      * @version 1.0.0
      * @module DGProjectDetector
      */
-    it("when server return projects", function () {
+    it("should fire a projectsloaded event ", function () {
         map.on('projectsloaded', spy);
 
-        expect(spy).toHaveBeenCalled();
+        waitsFor(function () {
+            return spy.callCount === 1;
+        });
+
+        runs(function () {
+            expect(spy).toHaveBeenCalled();
+        });
     });
 
+    /**
+     * Проверка, что событие projectchange срабатывает при смене проекта
+     *
+     * - Проверяем, что вызвалось событие projectchange при смене проекта
+     *
+     * @author Dima Rudenko <dm.rudenko@2gis.kiev.ua>
+     * @version 1.0.0
+     * @module DGProjectDetector
+     */
+    it("should fire a projectchange event ", function () {
+        map.on('projectchange', spy);
+
+        window.setTimeout(function () {
+            map.setView([53.706875284187, 91.433050566441], 13);
+        }, 1000);
+
+        waitsFor(function () {
+            return spy.callCount === 1;
+        });
+
+        runs(function () {
+            expect(spy).toHaveBeenCalled();
+        });
+    });
+
+    /**
+     * Проверка, что метод getProjectsList() события projectsloaded  возвращает данные
+     *
+     * - Подписываемся на событие "projectsloaded"
+     * - В полученном событии вызываем метод и роверяем что значение определено
+     *
+     * @author Dima Rudenko <dm.rudenko@2gis.kiev.ua>
+     * @version 1.0.0
+     * @module DGProjectDetector
+     */
+    describe("#getProjectsList", function () {
+        it("should be return not empty", function () {
+            map.on('projectsloaded', spy);
+
+            waitsFor(function () {
+                return spy.callCount === 1;
+            });
+
+            runs(function () {
+                expect(spy.mostRecentCall.args[0].getProjectsList()).toBeDefined();
+            });
+        });
+    });
+
+    /**
+     * Проверка, что метод getProject() возвращает корректный проект
+     *
+     * - Подписываемся на событие "projectchange"
+     * - Вызываем метод и роверяем что значение определено
+     *
+     * @author Dima Rudenko <dm.rudenko@2gis.kiev.ua>
+     * @version 1.0.0
+     * @module DGProjectDetector
+     */
+    describe("#getProject", function () {
+        it("should be return correct project", function () {
+            map.on('projectchange', spy);
+
+            window.setTimeout(function () {
+                map.setView([53.706875284187, 91.433050566441], 13);
+            }, 1000);
+
+            waitsFor(function () {
+                return spy.callCount === 1;
+            });
+
+            runs(function () {
+                var project  = spy.mostRecentCall.args[0].getProject();
+                expect(project.name).toContain('Абакан');
+            });
+        });
+    });
 });
