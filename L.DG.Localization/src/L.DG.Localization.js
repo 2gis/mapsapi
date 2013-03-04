6abcce9f3b87bd221394f8d1b0d0fae490c5e6c2
Dima Rudenko <dm.rudenko@2gis.kiev.ua>
2013-03-04 19:43:30 +0200

    JSAPI-2543: new plugins Localization

---
+++ L.DG.Localization/src/L.DG.Localization.js
@@ -0,0 +1,15 @@
+/**
+ * Leaflet DG Localization
+ * The plugin to provide localization.
+ *
+ * Version 1.0.0
+ *
+ * Copyright (c) 2013, 2GIS, Dima Rudenko
+ */
+
+L.DG = L.DG || {};
+
+L.DG.Localization = L.Class.extend({
+
+
+});
