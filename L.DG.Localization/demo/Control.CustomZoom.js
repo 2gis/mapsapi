23fb9b2a33747e4d6f04895553461c448900057e
Dima Rudenko <dm.rudenko@2gis.kiev.ua>
2013-03-05 17:26:04 +0200

    JSAPI-2543: update localization prototype

--- L.DG.Localization/demo/Control.CustomZoom.js
+++ L.DG.Localization/demo/Control.CustomZoom.js
@@ -2,41 +2,49 @@
  * Demo plugin for testing localization.
  * Overrides L.Control.Zoom
  */
-
+"use strict";
 L.Control.CustomZoom = L.Control.extend({
-    options: {
-        position: 'topleft'
+    options:{
+        position:'topleft'
     },
 
-    onAdd: function (map) {
+    initialize:function (option) {
+        L.Control.prototype.initialize.call(option);
+        this._ = L.Util.bind(L.DG.Localization.prototype._, this);
+        this.Dictionary =  L.extend(L.Control.CustomZoom.Dictionary);
+    },
+
+    onAdd:function (map) {
         var zoomName = 'leaflet-control-zoom',
             container = L.DomUtil.create('div', zoomName + ' leaflet-bar');
 
         this._map = map;
 
         this._zoomInButton  = this._createButton(
-            '+', 'Zoom in gg',  zoomName + '-in',  container, this._zoomIn,  this);
+            '+', this._("Zoom in",null,{msg: "(" + this._("Current Zoom") + ": " + this._map.getZoom() + ")"}),  zoomName + '-in',  container, this._zoomIn,  this);
         this._zoomOutButton = this._createButton(
-            '-', 'Zoom out', zoomName + '-out', container, this._zoomOut, this);
+            '-', this._("Zoom out"), zoomName + '-out', container, this._zoomOut, this);
 
         map.on('zoomend zoomlevelschange', this._updateDisabled, this);
+
+        map.on('langchange', this._updateLocale, this);
 
         return container;
     },
 
-    onRemove: function (map) {
+    onRemove:function (map) {
         map.off('zoomend zoomlevelschange', this._updateDisabled, this);
     },
 
-    _zoomIn: function (e) {
+    _zoomIn:function (e) {
         this._map.zoomIn(e.shiftKey ? 3 : 1);
     },
 
-    _zoomOut: function (e) {
+    _zoomOut:function (e) {
         this._map.zoomOut(e.shiftKey ? 3 : 1);
     },
 
-    _createButton: function (html, title, className, container, fn, context) {
+    _createButton:function (html, title, className, container, fn, context) {
         var link = L.DomUtil.create('a', className, container);
         link.innerHTML = html;
         link.href = '#';
@@ -54,7 +62,7 @@ L.Control.CustomZoom = L.Control.extend({
         return link;
     },
 
-    _updateDisabled: function () {
+    _updateDisabled:function () {
         var map = this._map,
             className = 'leaflet-disabled';
 
@@ -67,11 +75,17 @@ L.Control.CustomZoom = L.Control.extend({
         if (map._zoom === map.getMaxZoom()) {
             L.DomUtil.addClass(this._zoomInButton, className);
         }
+    },
+
+    _updateLocale:function () {
+        this._zoomInButton.title = this._("Zoom in");
+        this._zoomOutButton.title = this._("Zoom out");
+        this._updateDisabled();
     }
 });
 
 L.Map.mergeOptions({
-    zoomControl: true
+    zoomControl:true
 });
 
 L.control.zoom = function (options) {
--- L.DG.Localization/src/L.DG.Localization.js
+++ L.DG.Localization/src/L.DG.Localization.js
@@ -6,10 +6,53 @@
  *
  * Copyright (c) 2013, 2GIS, Dima Rudenko
  */
+"use strict";
 
 L.DG = L.DG || {};
-
 L.DG.Localization = L.Class.extend({
+    options:{
+        position:'topleft',
+        currentLang:"ru"
+    },
 
+    initialize:function (map) {
+        this._map = map;
+    },
 
+    _:function (msg, n, templ) {
+        var result,
+            lang = this._map.locale.getLang();
+
+        if(Object.prototype.toString.call(n) === '[object Number]') {
+           result = this.Dictionary[lang][msg][n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+        } else {
+            result = this.Dictionary[lang][msg];
+        }
+
+        if (templ){
+            result = L.Util.template(result, templ);
+        }
+        return result ? result : msg;
+    },
+
+    setLang:function (lang) {
+        if(lang){
+            this._map.options.currentLang = lang;
+            this._map.fire("langchange", {"lang":lang});
+        }
+    },
+
+    getLang:function () {
+        return this._map.options.currentLang;
+    }
+});
+
+L.Map.mergeOptions({
+    currentLang:"ru"
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.currentLang) {
+        this.locale = new L.DG.Localization(this);
+    }
 });
