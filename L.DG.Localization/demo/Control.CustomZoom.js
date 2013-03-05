5db3fcbb167a2a21fbbdbb484cb5047876b438c6
Dima Rudenko <dm.rudenko@2gis.kiev.ua>
2013-03-05 20:59:26 +0200

    JSAPI-2543: refactor method translate

--- L.DG.Localization/demo/Control.CustomZoom.js
+++ L.DG.Localization/demo/Control.CustomZoom.js
@@ -3,15 +3,14 @@
  * Overrides L.Control.Zoom
  */
 "use strict";
-L.Control.CustomZoom = L.Control.extend({
+L.Control.CustomZoom = L.Control.Zoom.extend({
     options:{
         position:'topleft'
     },
 
     initialize:function (option) {
         L.Control.prototype.initialize.call(option);
-        this._ = L.Util.bind(L.DG.Localization.prototype._, this);
-        this.Dictionary =  L.extend(L.Control.CustomZoom.Dictionary);
+        this._useLocalization();
     },
 
     onAdd:function (map) {
@@ -20,8 +19,8 @@ L.Control.CustomZoom = L.Control.extend({
 
         this._map = map;
 
-        this._zoomInButton  = this._createButton(
-            '+', this._("Zoom in",null,{msg: "(" + this._("Current Zoom") + ": " + this._map.getZoom() + ")"}),  zoomName + '-in',  container, this._zoomIn,  this);
+        this._zoomInButton = this._createButton(
+            '+', this._("Zoom in", {msg:"(" + this._("Current Zoom") + ": " + this._map.getZoom() + ")"}), zoomName + '-in', container, this._zoomIn, this);
         this._zoomOutButton = this._createButton(
             '-', this._("Zoom out"), zoomName + '-out', container, this._zoomOut, this);
 
@@ -32,53 +31,13 @@ L.Control.CustomZoom = L.Control.extend({
         return container;
     },
 
-    onRemove:function (map) {
-        map.off('zoomend zoomlevelschange', this._updateDisabled, this);
-    },
-
-    _zoomIn:function (e) {
-        this._map.zoomIn(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomOut:function (e) {
-        this._map.zoomOut(e.shiftKey ? 3 : 1);
-    },
-
-    _createButton:function (html, title, className, container, fn, context) {
-        var link = L.DomUtil.create('a', className, container);
-        link.innerHTML = html;
-        link.href = '#';
-        link.title = title;
-
-        var stop = L.DomEvent.stopPropagation;
-
-        L.DomEvent
-            .on(link, 'click', stop)
-            .on(link, 'mousedown', stop)
-            .on(link, 'dblclick', stop)
-            .on(link, 'click', L.DomEvent.preventDefault)
-            .on(link, 'click', fn, context);
-
-        return link;
-    },
-
-    _updateDisabled:function () {
-        var map = this._map,
-            className = 'leaflet-disabled';
-
-        L.DomUtil.removeClass(this._zoomInButton, className);
-        L.DomUtil.removeClass(this._zoomOutButton, className);
-
-        if (map._zoom === map.getMinZoom()) {
-            L.DomUtil.addClass(this._zoomOutButton, className);
-        }
-        if (map._zoom === map.getMaxZoom()) {
-            L.DomUtil.addClass(this._zoomInButton, className);
-        }
+    _useLocalization:function () {
+        this._ = L.Util.bind(L.DG.Localization.prototype._, this);
+        this.Dictionary = L.extend(L.Control.CustomZoom.Dictionary);
     },
 
     _updateLocale:function () {
-        this._zoomInButton.title = this._("Zoom in");
+        this._zoomInButton.title = this._("Zoom in", {msg:"(" + this._("Current Zoom") + ": " + this._map.getZoom() + ")"});
         this._zoomOutButton.title = this._("Zoom out");
         this._updateDisabled();
     }
--- L.DG.Localization/src/L.DG.Localization.js
+++ L.DG.Localization/src/L.DG.Localization.js
@@ -19,24 +19,33 @@ L.DG.Localization = L.Class.extend({
         this._map = map;
     },
 
-    _:function (msg, n, templ) {
+    _:function (msg) { // (String) || (String, args...)
         var result,
-            lang = this._map.locale.getLang();
+            tmpl,
+            lang = this._map.locale.getLang(),
+            args = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : null;
 
-        if(Object.prototype.toString.call(n) === '[object Number]') {
-           result = this.Dictionary[lang][msg][n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+        if (args) {
+            result = this.Dictionary[lang][msg];
+            for (var i = 0, len = args.length; i < len; i++) {
+                if (Object.prototype.toString.call(args[i]) === '[object Number]') {
+                    var n = args[i];
+                    result = this.Dictionary[lang][msg][n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+                }
+                if (Object.prototype.toString.call(args[i]) === '[object Object]') {
+                    tmpl = args[i];
+                }
+            }
+            result = tmpl ? L.Util.template(result, tmpl) : result;
         } else {
             result = this.Dictionary[lang][msg];
         }
 
-        if (templ){
-            result = L.Util.template(result, templ);
-        }
         return result ? result : msg;
     },
 
     setLang:function (lang) {
-        if(lang){
+        if (lang) {
             this._map.options.currentLang = lang;
             this._map.fire("langchange", {"lang":lang});
         }
