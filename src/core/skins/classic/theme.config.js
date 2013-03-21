d893c5fc3bdb1f5a440d49e30e7235693a44a554
Peter Dmitrienko <peter.dmitrienko@gmail.com>
2013-03-21 15:35:26 +0700

    JSAPI-2837: Added themes.

---
+++ src/core/skins/classic/theme.config.js
@@ -0,0 +1,24 @@
+var apiTheme = {
+    name: 'classic',
+
+    balloonOptions: {
+        offset: [139, -18]
+    },
+
+    markersData: {
+        defaultMarker: {
+            iconSize: [15, 23],
+            popupAnchor: [26, 27],
+            animation: true
+        }
+    },
+
+    controls: {
+        zoom: {
+            position: 'topleft'
+        },
+        whereami: {
+            position: 'topright'
+        }
+    }
+};
