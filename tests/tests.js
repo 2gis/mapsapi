a176f412387e51e35aad14a65d6739d13a4b33b4
AndreyChizh <info.andreychizh@gmail.com>
2013-03-26 12:23:58 +0200

    Fix build

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,36 @@
 var karma = require('karma');
 
-var server = require('./server.js'),
+var jsonpServer = require('./jsonpServer.js'),
     testConfig = {configFile : __dirname + '/karma.conf.js'};
 
 exports.run = function () {
-    server.init();
+    jsonpServer.init();
+
+    testConfig.browsers = ['PhantomJS'];
+
+    if (isArgv('--chrome')) {
+        testConfig.browsers.push('Chrome');
+    }
+    if (isArgv('--ff')) {
+        testConfig.browsers.push('Firefox');
+    }
+    if (isArgv('--opera')) {
+        testConfig.browsers.push('Opera');
+    }
+    if (isArgv('--safari')) {
+        testConfig.browsers.push('Safari');
+    }
+    if (isArgv('--ie')) {
+        testConfig.browsers.push('IE');
+    }
+
     karma.server.start(testConfig);
 };
 
 exports.server = function () {
     server.init();
 };
+
+function isArgv(optName) {
+    return process.argv.indexOf(optName) !== -1;
+}
