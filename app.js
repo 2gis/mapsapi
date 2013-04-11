bd95cb2231a8cb45f25b81af05fdd4bc9407b2de
AndreyChizh <info.andreychizh@gmail.com>
2013-04-11 14:23:26 +0300

    Fix web app

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ app.use(express.static(__dirname + '/public'));
  * Routes
  */
 app.all(/^\/(js|css)$/, function(req, resp, next) {
-    req.params.pkg = req.query.load || null;
-    req.params.isDebug = req.query.mode === 'debug';
-    req.params.skin = req.query.skin;
-    req.params.isIE = req.query.ie || false;
-    var contentType = req.path == '/js' ? 'application/x-javascript; charset=utf-8' : 'text/css';
-    req.callback = function(response, data) {
+    req.dgParams = {};
+    req.dgParams.pkg = req.query.load || null;
+    req.dgParams.isDebug = req.query.mode === 'debug';
+    req.dgParams.skin = req.query.skin;
+    req.dgParams.isIE = req.query.ie || false;
+    var contentType = (req.path == '/js') ? 'application/x-javascript; charset=utf-8' : 'text/css';
+    req.dgParams.callback = function(response, data) {
         response.set('Cache-Control', 'public, max-age=604800');
         response.set('X-Powered-By', '2GIS Maps API Server');
         response.set('Content-Type', contentType);
@@ -36,14 +37,14 @@ app.all(/^\/(js|css)$/, function(req, resp, next) {
 });
 
 app.get('/js', function(req, res){
-    build.getJS(req.params,function(data) {
-        req.callback(res, data);
+    build.getJS(req.dgParams,function(data) {
+        req.dgParams.callback(res, data);
     });
 });
 
 app.get('/css', function(req, res){
-    build.getCSS(req.params, function(data) {
-        req.callback(res, data);
+    build.getCSS(req.dgParams, function(data) {
+        req.dgParams.callback(res, data);
     });
 });
 
