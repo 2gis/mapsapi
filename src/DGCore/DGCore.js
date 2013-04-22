3e9e1e7f5144e0a929c0d91e907aacbe4ca7ab71
AndreyChizh <info.andreychizh@gmail.com>
2013-04-22 17:19:27 +0300

    JSAPI-2964 : Fix formatting

--- src/DGCore/DGCore.js
+++ src/DGCore/DGCore.js
@@ -20,10 +20,8 @@ L.Control.Zoom.prototype.onAdd = function (map) {
 
     this._map = map;
 
-    this._zoomInButton = this._createButton(
-        '+', 'Zoom in', zoomName + '__in', container, this._zoomIn, this);
-    this._zoomOutButton = this._createButton(
-        '-', 'Zoom out', zoomName + '__out', container, this._zoomOut, this);
+    this._zoomInButton = this._createButton('+', 'Zoom in', zoomName + '__in', container, this._zoomIn, this);
+    this._zoomOutButton = this._createButton('-', 'Zoom out', zoomName + '__out', container, this._zoomOut, this);
 
     map.on('zoomend zoomlevelschange', this._updateDisabled, this);
 
@@ -52,23 +50,19 @@ L.Control.Zoom.prototype.onAdd = function (map) {
 
     L.Popup.prototype._initLayout = function () {
         var prefix = 'leaflet-popup',
-            containerClass = prefix + ' ' + this.options.className + ' leaflet-zoom-' +
-                (this._animated ? 'animated' : 'hide'),
+            containerClass = prefix + ' ' + this.options.className + ' leaflet-zoom-' + (this._animated ? 'animated' : 'hide'),
             container = this._container = L.DomUtil.create('div', containerClass),
             closeButton;
 
         if (this.options.closeButton) {
-            closeButton = this._closeButton =
-                L.DomUtil.create('a', prefix + '-close-button', container);
+            closeButton = this._closeButton = L.DomUtil.create('a', prefix + '-close-button', container);
             closeButton.href = '#close';
             closeButton.innerHTML = '&#215;';
             L.DomEvent.disableClickPropagation(closeButton);
-
             L.DomEvent.on(closeButton, 'click', this._onCloseButtonClick, this);
         }
 
-        var wrapper = this._wrapper =
-            L.DomUtil.create('div', prefix + '-content-wrapper', container);
+        var wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
         L.DomEvent.disableClickPropagation(wrapper);
 
         this._contentNode = L.DomUtil.create('div', prefix + '-content dg-callout ', wrapper);
