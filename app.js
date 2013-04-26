a67d5a6a06971b3f4a77da27af8d5e623209722a
AndreyChizh <info.andreychizh@gmail.com>
2013-04-26 12:26:53 +0300

    Add comment

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(__dirname + '/public'));
  * Routes
  */
 app.all(/^\/(js|css)$/, function(req, resp, next) {
+    //@todo Add validations
     req.dgParams = {};
     req.dgParams.pkg = req.query.load || null;
     req.dgParams.isDebug = req.query.mode === 'debug';
