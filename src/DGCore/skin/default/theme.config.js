d014dc7ea89d0a890de1e4d4ff760b701e9ebee1
Peter Dmitrienko <p.dmitrienko@2gis.ru>
2013-05-14 09:51:02 +0700

    JSAPI-3043: Renovated marker animation.

--- src/DGCore/skin/default/theme.config.js
+++ src/DGCore/skin/default/theme.config.js
@@ -18,7 +18,8 @@ L.DG.configTheme = {
         popupAnchor: [0, 3],
         html: '<div class="dg-marker dg-marker_default dg-marker_animated"></div>',
         className: 'leaflet-div-icon',
-        iconAnchor: [7, 23]
+        iconAnchor: [7, 23],
+        animation: true
     },
 
     controls: {
