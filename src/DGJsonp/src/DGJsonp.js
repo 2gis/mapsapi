65395ec301178eeac172c6f26ea29313dcfa8b3d
Yarik Ponomarenko <yarik.ponomarenko@gmail.com>
2013-05-28 20:17:01 +0300

    JSAPI-2866 remove window.onerror from jsonp in order to debug

--- src/DGJsonp/src/DGJsonp.js
+++ src/DGJsonp/src/DGJsonp.js
@@ -11,10 +11,14 @@ L.DG.Jsonp = function (params) {
         callbackId, callbackName,
         url = params.url || '',
         data = params.data || {},
-        success = params.success || function () {},
-        error = params.error || function () {},
-        beforeSend = params.beforeSend || function () {},
-        complete = params.complete || function () {},
+        success = params.success || function () {
+        },
+        error = params.error || function () {
+        },
+        beforeSend = params.beforeSend || function () {
+        },
+        complete = params.complete || function () {
+        },
         timeout = params.timeout || 30 * 1000;
 
     head = document.getElementsByTagName('head')[0];
@@ -53,7 +57,7 @@ L.DG.Jsonp = function (params) {
     script.async = true;
     script.id = callbackId;
 
-    script.onerror = window.onerror = function (e) {
+    script.onerror = /*window.onerror = */function (e) { // @todo for debug, remove before ending of Geoclicker development
         error({ url: resUrl, event: e });
         complete({ url: resUrl, event: e }, 'error');
         script.parentNode.removeChild(script);
@@ -68,7 +72,8 @@ L.DG.Jsonp = function (params) {
     function cancelCallback() {
         removeScript(callbackId);
         if (L.DG.Jsonp.callback.hasOwnProperty(callbackId)) {
-            L.DG.Jsonp.callback[callbackId] = function () {};
+            L.DG.Jsonp.callback[callbackId] = function () {
+            };
         }
     }
 
