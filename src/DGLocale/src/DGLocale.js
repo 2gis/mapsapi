3499555cd3bd739f873767f83e1ca30d6c5384b7
a.lubchuk <a.lubchuk@2gis.kiev.ua>
2013-05-30 13:52:11 +0300

    JSAPI-2543 change scope of map object to global (in demo), add russian as default lang if incorrect lang was set

--- src/DGLocale/src/DGLocale.js
+++ src/DGLocale/src/DGLocale.js
@@ -6,35 +6,29 @@ L.DG.Locale = {
     t: function (msg, argument) { // (String, Number) -> String
         var result,
             lang = this._map.getLang(),
-            langIsset = false,
             msgIsset = false,
             dictionaryMsg,
             isNumeric,
             exp;
-
-        if(typeof this.constructor.Dictionary[lang] !== 'undefined') {
-            langIsset = true;
+        if (typeof this.constructor.Dictionary[lang] === 'undefined') {
+            lang = "ru";
+            this._map.setLang("ru");
+        } 
+        dictionaryMsg = this.constructor.Dictionary[lang][msg];
+        msgIsset = typeof dictionaryMsg !== 'undefined';
+        if (!msgIsset) {
+            return msg;
         }
-        if (langIsset) {
-            dictionaryMsg = this.constructor.Dictionary[lang][msg];
-            msgIsset = typeof dictionaryMsg !== 'undefined';
-            if (!msgIsset) {
-                return msg;
-            }
-        }
-        result = langIsset && msgIsset ? dictionaryMsg : msg;
+        result = msgIsset ? dictionaryMsg : msg;
         
         if (argument) {
             isNumeric = Object.prototype.toString.call(argument) === '[object Number]';
             if (!isNumeric) {
                 argument = 0;
             }
-            if (langIsset) {
-                exp = this.constructor.Dictionary[lang].pluralRules(argument);
-                result = dictionaryMsg[exp];
-            }
+            exp = this.constructor.Dictionary[lang].pluralRules(argument);
+            result = dictionaryMsg[exp];
         }
-        
         
         result = L.Util.template(result, {n: argument});
         return result ? result : msg;
