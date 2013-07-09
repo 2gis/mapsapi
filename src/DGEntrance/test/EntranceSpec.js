11b5155ba8bd121674f7ecbb25ba7a698fa0c1ba
AndreyGeonya <a.geonya@gmail.com>
2013-07-09 14:12:14 +0300

    JSAPI-3085, entrance tests added

--- src/DGEntrance/test/EntranceSpec.js
+++ src/DGEntrance/test/EntranceSpec.js
@@ -1,20 +1,61 @@
 describe('L.DG.Entrance', function () {
     var map,
+        spy,
+        entrance,
         mapContainer = document.createElement("div");
 
+    beforeEach(function () {
+        map = new L.Map(mapContainer, {
+            center: new L.LatLng(69.349552990994837, 87.75222519148015),
+            zoom: 17
+        });
+        entrance = new L.DG.Entrance({
+            "is_primary":false,
+            "name":"ЗАГС",
+            "points":["POINT(87.75222519148015 69.349552990994837)"],
+            "vectors":["LINESTRING(87.752433542237128 69.349501774294012,87.75222519148015 69.349552990994837)"]
+        });
+        entrance.addTo(map);
+    });
+
     afterEach(function () {
+        entrance.removeFrom(map);
         map && map.remove();
         map = null;
+    });
+
+    describe("#addTo and #removeFrom", function() {
+        it('should return "L.DG.Entrance" instance', function () {
+            var entrance1 = new L.DG.Entrance({
+                "is_primary":false,
+                "name":"ЗАГС",
+                "points":["POINT(87.75222519148015 69.349552990994837)"],
+                "vectors":["LINESTRING(87.752433542237128 69.349501774294012,87.75222519148015 69.349552990994837)"]
+            });
+            expect(entrance1.addTo(map)).to.be.a(L.DG.Entrance);
+            expect(entrance1.removeFrom(map)).to.be.a(L.DG.Entrance);
+        });
     });
 
     describe("#show", function() {
 
         it('should return "L.DG.Entrance" instance', function () {
-
+            expect(entrance.show()).to.be.a(L.DG.Entrance);
         });
 
-        it('should fire "entranceshow" event on map', function () {
+        it('should fire "dgEntranceShow" event', function () {
+            spy = sinon.spy();
+            map.on('dgEntranceShow', spy);
+            entrance.show();
+            expect(spy.called).to.be.ok();
+        });
 
+        it('should not fire "dgEntranceShow" event if already shown', function () {
+            entrance.show();
+            spy = sinon.spy();
+            map.on('dgEntranceShow', spy);
+            entrance.show();
+            expect(spy.called).not.to.be.ok();
         });
 
     });
@@ -22,41 +63,51 @@ describe('L.DG.Entrance', function () {
     describe("#hide", function() {
 
         it('should return "L.DG.Entrance" instance', function () {
+            expect(entrance.hide()).to.be.a(L.DG.Entrance);
+        });     
 
+        it('should fire "dgEntranceHide" event', function () {
+            entrance.show();
+            spy = sinon.spy();
+            map.on('dgEntranceHide', spy);
+            entrance.hide();
+            expect(spy.called).to.be.ok();
         });
 
-        it('should fire "entrancehide" event on map', function () {
-
-        });
-
-    });
-
-    // оставить только один
-
-    describe("#shouldBeShown", function() {
-
-        it('should be true, if zoom > 16', function () {
-
-        });
-
-        it('should be false, if zoom < 16', function () {
-
+        it('should not fire "dgEntranceHide" event if already hidden (hidden by default)', function () {
+            spy = sinon.spy();
+            map.on('dgEntranceHide', spy);
+            entrance.hide();
+            expect(spy.called).not.to.be.ok();
         });
 
     });
 
-    describe("#mapShouldBeZoomed", function() {
+    describe("#isShown", function() {
 
-        it('should zoom in map if zoom < 16', function () {
+        it('should return "false" by default', function () {
+            expect(entrance.isShown()).not.to.be.ok();
+        });
 
+        it('should return "true" after show and "false" after hide', function () {
+            entrance.show();
+            expect(entrance.isShown()).to.be.ok();
+            entrance.hide();
+            expect(entrance.isShown()).not.to.be.ok();
         });
 
     });
 
     describe("#getBounds", function() {
 
-        it('bounds should be correct', function () {
+        it('should return bounds of all arrows of the entrance', function () {
+            var entranceBounds = new L.LatLngBounds(
+                [69.349552990994837, 87.75222519148015],
+                [69.349501774294012, 87.752433542237128]
+            );
+            expect(entrance.getBounds()).to.eql(entranceBounds);
 
+            // TODO: test another entrance types
         });
 
     });
