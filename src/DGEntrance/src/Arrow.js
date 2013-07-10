acb3aa489aaf80ef2cf4625a16657b792fc3fb65
AndreyGeonya <a.geonya@gmail.com>
2013-07-10 21:12:11 +0300

    JSAPI-3085, first arrow marker added

--- src/DGEntrance/src/Arrow.js
+++ src/DGEntrance/src/Arrow.js
@@ -1,4 +1,5 @@
 L.DG.Entrance.Arrow = L.Polyline.extend({
+    _markerPath: null,
 
     initialize: function (latlngs, options) { // (Array, Object)
         var options = options || {},
@@ -7,8 +8,57 @@ L.DG.Entrance.Arrow = L.Polyline.extend({
         options.animation = [animation];
 
         L.Polyline.prototype.initialize.call(this, latlngs, options);
-    }
-    
+    },
+
+    onAdd: function (map) {
+        L.Polyline.prototype.onAdd.call(this, map);
+        this._addMarker();
+    },
+
+    _addMarker: function () {
+        var marker = this._createElement('marker', {
+            id: 'arrow-marker',
+            viewBox: '0 0 23 22',
+            refX: '12',
+            refY: '11',
+            markerUnits: 'userSpaceOnUse',
+            markerWidth: '23',
+            markerHeight: '22',
+            orient: 'auto'
+        });
+
+        this._markerPath = this._createElement('path');
+        
+        // Пример описания path-а контура стрелки:
+        // this._markerPath.setAttribute('d', 'M23,11.001c0-1.139-0.643-2.174-1.658-2.686l-16-7.998C4.188-0.261,2.792-0.034,1.879,0.88 l-1,1C0.293,2.466,0,3.233,0,4.002s0.293,1.536,0.879,2.12L4,9.244v3.515L0.879,15.88C0.293,16.466,0,17.231,0,18.001 c0,0.768,0.293,1.534,0.879,2.12l1,1c0.913,0.913,2.309,1.142,3.463,0.563l16-8C22.357,13.176,23,12.14,23,11.001L23,11.001z');
+        
+        this._markerPath.setAttribute('d', 'M20,11 L4,3 3,4 7,8 7,14 3,18 4,19z');
+
+        this._path.parentNode.appendChild(marker);
+        marker.appendChild(this._markerPath);
+        this._path.setAttribute('marker-end', 'url(#arrow-marker)');
+    },
+
+    _updateStyle: function () {
+        L.Polyline.prototype._updateStyle.call(this);
+
+        if (this._markerPath !== null) {
+            /*this._markerPath.setAttribute('stroke', this._path.getAttribute('stroke'));
+            this._markerPath.setAttribute('stroke-opacity', this._path.getAttribute('stroke-opacity'));
+            this._markerPath.setAttribute('stroke-width', this._path.getAttribute('stroke-width'));
+            if (this.options.dashArray) {
+                this._markerPath.setAttribute('stroke-dasharray', this._path.getAttribute('stroke-dasharray'));
+            }
+            else {
+                this._markerPath.removeAttribute('stroke-dasharray');
+            }*/
+            this._markerPath.setAttribute('fill', '#4b647d' /*this._path.getAttribute('fill')*/);
+            /*this._markerPath.setAttribute('fill-opacity', this._path.getAttribute('fill-opacity'));
+            this._markerPath.setAttribute('stroke-linejoin', this._path.getAttribute('stroke-linejoin'));
+            this._markerPath.setAttribute('stroke-linecap', this._path.getAttribute('stroke-linecap'));*/
+        };
+    },
+
 });
 
 L.DG.Entrance.arrow = function (latlngs, options) {
--- src/DGEntrance/src/ArrowAnimation.js
+++ src/DGEntrance/src/ArrowAnimation.js
@@ -15,7 +15,7 @@ L.Path.include({
 
         this.projectLatlngs();
         this._addAnimations();
-        this._addMarker();
+//        this._addMarker();
 
         if (this._container) {
             this._map._pathRoot.appendChild(this._container);
@@ -82,26 +82,72 @@ L.Path.include({
         this.animations[options.id] = animation;
     },
 
+    // TODO: refactor and move out this marker's code from this class!
+/*    _markerPath: null,
+
     _addMarker: function () {
         var marker = this._createElement('marker', {
-            id: 'Triangle',
-            viewBox: '0 0 10 10',
-            refX: '0',
-            refY: '5',
-            markerUnits: 'strokeWidth',
-            markerWidth: '4',
-            markerHeight: '3',
+            id: 'arrow-marker',
+            viewBox: '0 0 23 22',
+            refX: '12',
+            refY: '11',
+            markerUnits: 'userSpaceOnUse',
+            markerWidth: '23',
+            markerHeight: '22',
             orient: 'auto'
-        }),
-        markerPath = this._createElement('path', {
-            d: 'M 0 0 L 10 5 L 0 10 z'
         });
 
-        //console.log(this._path);
+        this._markerPath = this._createElement('path');
+        
+        // Это описание path-а контура стрелки:
+        // this._markerPath.setAttribute('d', 'M23,11.001c0-1.139-0.643-2.174-1.658-2.686l-16-7.998C4.188-0.261,2.792-0.034,1.879,0.88 l-1,1C0.293,2.466,0,3.233,0,4.002s0.293,1.536,0.879,2.12L4,9.244v3.515L0.879,15.88C0.293,16.466,0,17.231,0,18.001 c0,0.768,0.293,1.534,0.879,2.12l1,1c0.913,0.913,2.309,1.142,3.463,0.563l16-8C22.357,13.176,23,12.14,23,11.001L23,11.001z');
+        // Это описание path-а самой стрелки:
+        this._markerPath.setAttribute('d', 'M20,11 L4,3 3,4 7,8 7,14 3,18 4,19z');
+
         this._path.parentNode.appendChild(marker);
-        marker.appendChild(markerPath);
-        this._path.setAttribute('marker-end', 'url(#Triangle)');
+        marker.appendChild(this._markerPath);
+        this._path.setAttribute('marker-end', 'url(#arrow-marker)');
     },
+
+    _updateStyle: function () {
+        // leaflet code
+        if (this.options.stroke) {
+            this._path.setAttribute('stroke', this.options.color);
+            this._path.setAttribute('stroke-opacity', this.options.opacity);
+            this._path.setAttribute('stroke-width', this.options.weight);
+            if (this.options.dashArray) {
+                this._path.setAttribute('stroke-dasharray', this.options.dashArray);
+            } else {
+                this._path.removeAttribute('stroke-dasharray');
+            }
+        } else {
+            this._path.setAttribute('stroke', 'none');
+        }
+        if (this.options.fill) {
+            this._path.setAttribute('fill', this.options.fillColor || this.options.color);
+            this._path.setAttribute('fill-opacity', this.options.fillOpacity);
+        } else {
+            this._path.setAttribute('fill', 'none');
+        }
+
+        // marker path update
+        if (this._markerPath !== null) {
+            /*this._markerPath.setAttribute('stroke', this._path.getAttribute('stroke'));
+            this._markerPath.setAttribute('stroke-opacity', this._path.getAttribute('stroke-opacity'));
+            this._markerPath.setAttribute('stroke-width', this._path.getAttribute('stroke-width'));
+            if (this.options.dashArray) {
+                this._markerPath.setAttribute('stroke-dasharray', this._path.getAttribute('stroke-dasharray'));
+            }
+            else {
+                this._markerPath.removeAttribute('stroke-dasharray');
+            }*/
+            //this._markerPath.setAttribute('fill', '#4b647d' /*this._path.getAttribute('fill')*/);
+            /*this._markerPath.setAttribute('fill-opacity', this._path.getAttribute('fill-opacity'));
+            this._markerPath.setAttribute('stroke-linejoin', this._path.getAttribute('stroke-linejoin'));
+            this._markerPath.setAttribute('stroke-linecap', this._path.getAttribute('stroke-linecap'));*/
+/*        };
+    },*/
+    // END TODO
 
     _createElement: function (type, options) {
         var options = options || {},
