174db1948fd45f8d9a8e93ab9967b0a06c519377
AndreyGeonya <a.geonya@gmail.com>
2013-07-27 15:16:36 +0300

    JSAPI-3085, vml arrow fixed on zoomend

--- src/DGEntrance/src/ArrowVml.js
+++ src/DGEntrance/src/ArrowVml.js
@@ -3,6 +3,22 @@ if (L.Browser.vml) {
 
     L.DG.Entrance.Arrow.include({
 
+        _endArrowStroke: null,
+
+        onAdd: function (map){ // (L.Map)
+            L.Polyline.prototype.onAdd.call(this, map);
+
+            map.on({'zoomend': this._updateEndArrow}, this);
+            map.on({'zoomend': this._updateStyle}, this);
+        },
+
+        onRemove: function (map){ // (L.Map)
+            L.Polyline.prototype.onRemove.call(this, map);
+
+            map.off({'zoomend': this._updateEndArrow}, this);
+            map.off({'zoomend': this._updateStyle}, this);
+        },
+
         _initElements: function () {
             this._map._initPathRoot();
             this._initPath();
@@ -11,20 +27,35 @@ if (L.Browser.vml) {
         },
 
         _initEndArrow: function () {
+            this._endArrowStroke = this._createElement('stroke');
+            this._updateEndArrow();
+            this._container.appendChild(this._endArrowStroke);
+        },
+
+        _updateEndArrow: function () {
             var byZoom = this.options.byZoom,
-                zoom = this._map.getZoom(),
-                stroke = this._createElement('stroke');
+                zoom = this._map.getZoom();
 
             if (typeof byZoom[zoom] !== 'undefined' &&
                 typeof byZoom[zoom].vmlEndArrow !== 'undefined') {
                 
-                stroke.endarrow = byZoom[zoom].vmlEndArrow;
+                this._endArrowStroke.endarrow = byZoom[zoom].vmlEndArrow;
             }
             else {
-                stroke.endarrow = 'classic';
+                this._endArrowStroke.endarrow = 'classic';
             }
-            
-            this._container.appendChild(stroke);
+        },
+
+        _updateStyle: function () {
+            var optionsByZoom =  this.options.byZoom,
+                zoom = this._map.getZoom();
+
+            L.Polyline.prototype._updateStyle.call(this);
+
+            if (typeof optionsByZoom[zoom] !== 'undefined' &&
+                typeof optionsByZoom[zoom].weight !== 'undefined') {
+                this._stroke.weight = optionsByZoom[zoom].weight + 'px';
+            }
         }
 
     });
