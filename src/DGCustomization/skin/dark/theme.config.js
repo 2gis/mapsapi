362ae6a4adcbf51bac437f43f6178e3c6fb41456
AndreyGeonya <a.geonya@gmail.com>
2013-10-01 18:09:14 +0300

    JSAPI-3283, init dark skin

---
+++ src/DGCustomization/skin/dark/theme.config.js
@@ -0,0 +1,32 @@
+L.DG = L.DG || {};
+L.DG.configTheme = {
+    name: 'dark',
+
+    balloonOptions: {
+        offset: {
+            x: 0,
+            y: -47
+        }
+    },
+
+    markersData: {
+        iconSize: [16, 24],
+        popupAnchor: [0, 3],
+        html: '<div class="dg-marker dg-marker_default dg-marker_animated"></div>',
+        className: 'leaflet-div-icon',
+        iconAnchor: [7, 23],
+        animation: true
+    },
+
+    controls: {
+        zoom: {
+            position: 'topleft'
+        },
+        fullScreen: {
+            position: 'topright'
+        },
+        whereami: {
+            position: 'topright'
+        }
+    }
+};
