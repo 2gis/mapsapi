9ca206ca09224d5552b684c2b1689da086d84987
Dmitry Lutsik <otoyto@gmail.com>
2013-10-03 16:47:33 +0300

    fix jshint warnings

--- src/DGEntrance/src/ArrowSvgAnimationOptions.js
+++ src/DGEntrance/src/ArrowSvgAnimationOptions.js
@@ -18,11 +18,11 @@ if (L.Browser.svg) {
             return animateArrow;
         },
 
-        _getAnimationValues: function(verticesCount) {
+        _getAnimationValues: function (verticesCount) {
             return (verticesCount === 2) ? this._getShakeAnimationValues : this._getSlideAnimationValues;
         },
 
-        _getSlideAnimationValues: function(points) { // (Array) -> String
+        _getSlideAnimationValues: function (points) { // (Array) -> String
             var d = '',
                 prevPoint = '',
                 curPoint = '',
@@ -31,24 +31,24 @@ if (L.Browser.svg) {
 
             d = M + ' ' + l + '; ';
             for (var i = 1; i < points.length; i++) {
-                curPoint += (points[i].x - points[i-1].x) + ' ' + (points[i].y - points[i-1].y);
+                curPoint += (points[i].x - points[i - 1].x) + ' ' + (points[i].y - points[i - 1].y);
 
                 if (i === points.length - 1) {
-                    curPoint = (points[i].x - points[i-1].x) + ' ' + (points[i].y - points[i-1].y);
+                    curPoint = (points[i].x - points[i - 1].x) + ' ' + (points[i].y - points[i - 1].y);
                     d += M + ' l ' + prevPoint  + curPoint + ';';
                     break;
                 }
 
                 d += M + ' l ' + curPoint + '; ';
-                d += M + ' l ' + curPoint + ' ' + l +'; ';
+                d += M + ' l ' + curPoint + ' ' + l + '; ';
                 curPoint += ' l ';
-                prevPoint += (points[i].x - points[i-1].x) + ' ' + (points[i].y - points[i-1].y) + ' l ';
+                prevPoint += (points[i].x - points[i - 1].x) + ' ' + (points[i].y - points[i - 1].y) + ' l ';
             }
 
             return d;
         },
 
-        _getShakeAnimationValues: function(points) { // (Array) -> String
+        _getShakeAnimationValues: function (points) { // (Array) -> String
             var d = '', px0, py0,
                 // config coefficient values for arrow animation
                 relDiff = [1, 0.4, 1, 0.84, 1, 0.94, 1],
@@ -61,7 +61,7 @@ if (L.Browser.svg) {
 
             d = 'M ' + px0 + ' ' + py0 + l + '; ';
             for (var i = 0; i < relDiff.length; i++) {
-                d += ' M ' + (px0 + dx*relDiff[i]) + ' ' + (py0 + dy*relDiff[i]) + l + ';';
+                d += ' M ' + (px0 + dx * relDiff[i]) + ' ' + (py0 + dy * relDiff[i]) + l + ';';
             }
 
             return d;
@@ -70,7 +70,7 @@ if (L.Browser.svg) {
         _getPolylineLength: function (latlngs) {
             var len = 0;
             for (var i = 1; i < latlngs.length; i++) {
-                len += latlngs[i-1].distanceTo(latlngs[i]);
+                len += latlngs[i - 1].distanceTo(latlngs[i]);
             }
             return len;
         },
@@ -83,40 +83,40 @@ if (L.Browser.svg) {
                 segmentLength;
 
             if (latlngs.length === 2) {
-                result = "0; 0.33; 0.495; 0.66; 0.77; 0.88; 0.935; 1";
+                result = '0; 0.33; 0.495; 0.66; 0.77; 0.88; 0.935; 1';
             }
             else if (latlngs.length === 3) {
-                result = "0; 0.33; 0.34; 1";
+                result = '0; 0.33; 0.34; 1';
             }
             else if (latlngs.length === 4) {
-                result = "0; 0.25; 0.26; 0.5; 0.51; 1";
+                result = '0; 0.25; 0.26; 0.5; 0.51; 1';
             }
             else {
                 for (var i = 1; i < latlngs.length; i++) {
-                    segmentLength = latlngs[i-1].distanceTo(latlngs[i]);
+                    segmentLength = latlngs[i - 1].distanceTo(latlngs[i]);
                     segmentRatio = segmentLength / polyLen;
 
-                    resultArr.push(resultArr[resultArr.length-1] + segmentRatio);
+                    resultArr.push(resultArr[resultArr.length - 1] + segmentRatio);
 
                     if (i < latlngs.length - 1) {
                         // 2 points for each vertice (but not for first and last)
-                        resultArr.push(resultArr[resultArr.length-1]);
+                        resultArr.push(resultArr[resultArr.length - 1]);
                     }
                     else {
                         // last point should be 1, but some times it looks like 0.9999...
-                        resultArr[resultArr.length-1] = 1;
+                        resultArr[resultArr.length - 1] = 1;
                     }
                 }
                 result = resultArr.join('; ');
             }
-            
+
             return result;
         },
 
         _getAnimationTime: function (verticesCount) {
-            if (verticesCount === 2) return '0.7s';
-            else if (verticesCount === 3 || verticesCount === 4) return "0.5s";
-            else return "0.7s";
+            if (verticesCount === 2) { return '0.7s'; }
+            else if (verticesCount === 3 || verticesCount === 4) { return '0.5s'; }
+            else { return '1.7s'; }
         }
     });
 }
