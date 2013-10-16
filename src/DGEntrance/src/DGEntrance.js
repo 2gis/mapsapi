dd81a8ee712f5993dd2acd1d80a12f6d2782b862
Wenqer <wenqeer@gmail.com>
2013-10-16 12:10:50 +0300

    fixed bug in DGEntrance with zoom on arrow animation

--- src/DGEntrance/src/DGEntrance.js
+++ src/DGEntrance/src/DGEntrance.js
@@ -128,12 +128,12 @@ L.DG.Entrance = L.Class.extend({
         var map = this._map,
             maxZoom;
 
-        map.once('moveend', function () {
-            if (this._map && !this._isAllowedZoom()) {
+        if (this._map && !this._isAllowedZoom()) {
+            map.once('moveend', function () {
                 maxZoom = map.dgProjectDetector.getProject().max_zoom_level;
                 map.setZoom(maxZoom, {animate: false});
-            }
-        }, this);
+            }, this);
+        }
 
         if (!map.getBounds().contains(this.getBounds()) || !this._isAllowedZoom()) {
             map.panTo(this.getBounds().getCenter(), {animate: false});
@@ -141,7 +141,7 @@ L.DG.Entrance = L.Class.extend({
     },
 
     _isAllowedZoom: function () {
-        return !(this._map.getZoom() < L.DG.Entrance.SHOW_FROM_ZOOM);
+        return this._map.getZoom() > L.DG.Entrance.SHOW_FROM_ZOOM;
     },
 
     _getArrowStrokeOptions: function () {
