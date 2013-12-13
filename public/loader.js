d9d74c4186ffe871af6e1cd5d10222b7b42ff98a
Dmitry Lutsik <otoyto@gmail.com>
2013-12-13 16:24:03 +0200

    jshint errors

--- public/loader.js
+++ public/loader.js
@@ -1,23 +1,22 @@
-(function() {
+(function () {
     'use strict';
 
-    var onLoadJs = function(){},
+    var onLoadJs = function () {},
         baseURL,
         queryString,
-        params,
         version = 'v=8210d6';
 
     function processURL() {
         var scripts, scriptURL;
-        scripts = document.getElementsByTagName("script");
-        scriptURL = scripts[scripts.length-1].src;
+        scripts = document.getElementsByTagName('script');
+        scriptURL = scripts[scripts.length - 1].src;
         return scriptURL.split('?');
     }
 
     function getBaseURL() {
         var pattern = /loader.js/,
             url = processURL();
-        return (url[0]) ? url[0].replace(pattern, "") : '/';
+        return (url[0]) ? url[0].replace(pattern, '') : '/';
     }
 
     function getParamsURI() {
@@ -43,17 +42,17 @@
         if (!queryString) {
             return {};
         }
-        if (queryString[0] == "?") {
-            queryString = queryString.slice(1);    
+        if (queryString[0] === '?') {
+            queryString = queryString.slice(1);
         }
-        var paramString = queryString.split("&"),
+        var paramString = queryString.split('&'),
             length = paramString.length,
             buffer = {},
             param,
             i;
 
         for (i = 0; i < length; i++) {
-            param = paramString[i].split("=");
+            param = paramString[i].split('=');
             buffer[param[0]] = param[1];
         }
         return buffer;
@@ -67,19 +66,19 @@
         document.getElementsByTagName('head')[0].appendChild(css);
     }
 
-    function loadJS(link, callback) {
+    function loadJS(link) {
         var js = document.createElement('script');
         js.setAttribute('type', 'text/javascript');
         js.setAttribute('src', link);
         if (js.readyState) {
-            js.onreadystatechange = function() {
+            js.onreadystatechange = function () {
                 if (js.readyState === 'loaded' || js.readyState === 'complete') {
                     js.onreadystatechange = null;
                     onLoadJs();
                 }
             };
         } else {
-            js.onload = function() {
+            js.onload = function () {
                 onLoadJs();
             };
         }
@@ -87,7 +86,7 @@
     }
 
     window.L = window.L || {};
-    window.L.onLoad = function(callback) {
+    window.L.onLoad = function (callback) {
         onLoadJs = callback;
     };
 
@@ -100,11 +99,11 @@
     loadCSS(baseURL + 'css' + queryString);
     // load js on document ready
     if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             loadJS(baseURL + 'js' + queryString);
-        }, false );
+        }, false);
     } else if (document.attachEvent) {
-        document.attachEvent('onreadystatechange', function() {
+        document.attachEvent('onreadystatechange', function () {
             if (document.readyState === 'complete') {
                 loadJS(baseURL + 'js' + queryString);
             }
