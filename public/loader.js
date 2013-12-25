90d8c32c70abd0e591ebf1623fd7670711b65033
Dmitry Lutsik <otoyto@gmail.com>
2013-12-25 14:58:30 +0200

    remove console emulation ^_^

--- public/loader.js
+++ public/loader.js
@@ -1,25 +1,3 @@
-(function() {
-        var method;
-        var noop = function () {};
-        var methods = [
-            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-            'timeStamp', 'trace', 'warn'
-        ];
-        var length = methods.length;
-        var console = (window.console = window.console || {});
-
-        while (length--) {
-            method = methods[length];
-
-            if (!console[method]) {
-                console[method] = noop;
-            }
-        }
-    }());
-
-
 (function () {
     'use strict';
 
