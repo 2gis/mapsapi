0e5ff9246243e3c36d4dd3e69c904b12c5da0028
Dmitry Lutsik <otoyto@gmail.com>
2014-01-30 13:44:31 +0200

    gulp tmp_modules basic dist build

--- build/deps.js
+++ build/deps.js
@@ -2,16 +2,16 @@ var deps = {
 
     DGCore: {
         desc: 'Main module',
-        src: [
-            '../vendors/polyfills/json2.js',
-            '../vendors/polyfills/html5shiv.js',
-            '../vendors/polyfills/es5.js',
-            'DGCore/src/DGCore.js',
-            'DGCore/src/DGplugin.js',
-            'DGCore/src/DGthen.js'
+        js: [
+            '/vendors/polyfills/json2.js',
+            '/vendors/polyfills/html5shiv.js',
+            '/vendors/polyfills/es5.js',
+            'src/DGCore/src/DGCore.js',
+            'src/DGCore/src/DGplugin.js',
+            'src/DGCore/src/DGthen.js'
         ],
         css: {
-            all: ['../vendors/leaflet/dist/leaflet.css']
+            all: ['/vendors/leaflet/dist/leaflet.css']
         },
         heading: '2GIS modules',
         deps: [ 'Core',
@@ -55,68 +55,68 @@ var deps = {
 
     DGWhen: {
         desc: 'Promise/Deffered object module',
-        src: [ 'DGWhen/src/DGWhen.js']
+        js: [ 'src/DGWhen/src/DGWhen.js']
     },
 
     DGAjax: {
         desc: '2GIS Ajax module',
-        src: ['DGAjax/src/DGAjax.js'],
+        js: ['src/DGAjax/src/DGAjax.js'],
         deps: ['DGCore']
     },
 
     DGLabel: {
         desc: '2GIS Label module',
-        src: [
-            'DGLabel/src/DGLabel.js',
-            'DGLabel/src/Marker.DGLabel.js',
-            'DGLabel/src/Path.DGLabel.js'
+        js: [
+            'src/DGLabel/src/DGLabel.js',
+            'src/DGLabel/src/Marker.DGLabel.js',
+            'src/DGLabel/src/Path.DGLabel.js'
         ],
         css: {
-            all: ['DGLabel/skin/{skin}/css/DGLabel.css']
+            all: ['src/DGLabel/skin/{skin}/css/DGLabel.css']
         },
         deps: ['DGCore']
     },
 
     DGWkt: {
         desc: 'WKT parser module',
-        src: [
-            'DGWkt/Wkt.js',
-            'DGWkt/DGWkt.js'
+        js: [
+            'src/DGWkt/Wkt.js',
+            'src/DGWkt/DGWkt.js'
         ],
         deps: ['DGCore']
     },
 
     DGCustomization: {
         desc: 'LeafLet customization module',
-        src: [
-            'DGCustomization/skin/basic/skin.config.js',
-            '../vendors/baron/baron.js',
-            '../vendors/baron/js/bonzo.js',
-            '../vendors/baron/js/bean.js',
-            '../vendors/baron/js/qwery.js',
-            'DGCustomization/src/DGCustomization.js',
-            'DGCustomization/src/DGPopup.js',
-            'DGCustomization/src/DGZoom.js',
-            'DGCustomization/lang/DGZoom/ru.js',
-            'DGCustomization/lang/DGZoom/it.js',
-            'DGCustomization/lang/DGZoom/cs.js',
-            'DGCustomization/lang/DGZoom/en.js'
+        js: [
+            'src/DGCustomization/skin/basic/skin.config.js',
+            '/vendors/baron/baron.js',
+            '/vendors/baron/js/bonzo.js',
+            '/vendors/baron/js/bean.js',
+            '/vendors/baron/js/qwery.js',
+            'src/DGCustomization/src/DGCustomization.js',
+            'src/DGCustomization/src/DGPopup.js',
+            'src/DGCustomization/src/DGZoom.js',
+            'src/DGCustomization/lang/DGZoom/ru.js',
+            'src/DGCustomization/lang/DGZoom/it.js',
+            'src/DGCustomization/lang/DGZoom/cs.js',
+            'src/DGCustomization/lang/DGZoom/en.js'
         ],
         css: {
             all: [
-                'DGCustomization/skin/basic/css/leaflet-reset.css',
-                '../vendors/baron/baron.css',
-                'DGCustomization/skin/{skin}/css/zoom.css',
-                'DGCustomization/skin/{skin}/css/baron.css',
-                'DGCustomization/skin/{skin}/css/marker.css',
-                'DGCustomization/skin/{skin}/css/callout.css',
-                'DGCustomization/skin/{skin}/css/baron.css'
+                'src/DGCustomization/skin/basic/css/leaflet-reset.css',
+                '/vendors/baron/baron.css',
+                'src/DGCustomization/skin/{skin}/css/zoom.css',
+                'src/DGCustomization/skin/{skin}/css/baron.css',
+                'src/DGCustomization/skin/{skin}/css/marker.css',
+                'src/DGCustomization/skin/{skin}/css/callout.css',
+                'src/DGCustomization/skin/{skin}/css/baron.css'
             ],
             ie: [
-                'DGCustomization/skin/{skin}/css/baron.ie.css',
-                'DGCustomization/skin/{skin}/css/callout.ie.css',
-                'DGCustomization/skin/{skin}/css/zoom.ie.css',
-                'DGCustomization/skin/{skin}/css/marker.ie.css'
+                'src/DGCustomization/skin/{skin}/css/baron.ie.css',
+                'src/DGCustomization/skin/{skin}/css/callout.ie.css',
+                'src/DGCustomization/skin/{skin}/css/zoom.ie.css',
+                'src/DGCustomization/skin/{skin}/css/marker.ie.css'
             ]
         },
         deps: ['DGCore', 'DGLocale', 'DGRoundControl']
@@ -124,12 +124,12 @@ var deps = {
 
     DGAttribution: {
         desc: '2GIS copyright',
-        src: [
-            'DGAttribution/src/DGAttribution.js',
-            'DGAttribution/lang/ru.js',
-            'DGAttribution/lang/it.js',
-            'DGAttribution/lang/cs.js',
-            'DGAttribution/lang/en.js'
+        js: [
+            'src/DGAttribution/src/DGAttribution.js',
+            'src/DGAttribution/lang/ru.js',
+            'src/DGAttribution/lang/it.js',
+            'src/DGAttribution/lang/cs.js',
+            'src/DGAttribution/lang/en.js'
         ],
         css: {
             all: ['DGAttribution/skin/{skin}/css/DGAttribution.css'],
@@ -140,78 +140,78 @@ var deps = {
 
     DGLocale: {
         desc: 'Localization module',
-        src: [
-            'DGLocale/src/DGDictionary.js',
-            'DGLocale/src/DGLocale.js'
+        js: [
+            'src/DGLocale/src/DGDictionary.js',
+            'src/DGLocale/src/DGLocale.js'
         ],
         deps: ['DGCore']
     },
 
     DGLocation: {
         desc: 'Location control module',
-        src: [
-            'DGLocation/src/DGLocation.js',
-            'DGLocation/lang/ru.js',
-            'DGLocation/lang/it.js',
-            'DGLocation/lang/cs.js',
-            'DGLocation/lang/en.js'
+        js: [
+            'src/DGLocation/src/DGLocation.js',
+            'src/DGLocation/lang/ru.js',
+            'src/DGLocation/lang/it.js',
+            'src/DGLocation/lang/cs.js',
+            'src/DGLocation/lang/en.js'
         ],
         css: {
-            all: ['DGLocation/skin/{skin}/css/DGLocation.css']
+            all: ['src/DGLocation/skin/{skin}/css/DGLocation.css']
         },
         deps: ['DGCore', 'DGLocale', 'DGLabel', 'DGRoundControl']
     },
 
     DGFullScreen: {
         desc: 'Full screen module',
-        src: [
-            'DGFullScreen/src/DGFullScreen.js',
-            'DGFullScreen/lang/ru.js',
-            'DGFullScreen/lang/it.js',
-            'DGFullScreen/lang/cs.js',
-            'DGFullScreen/lang/en.js'
+        js: [
+            './src/DGFullScreen/src/DGFullScreen.js',
+            './src/DGFullScreen/lang/ru.js',
+            './src/DGFullScreen/lang/it.js',
+            './src/DGFullScreen/lang/cs.js',
+            './src/DGFullScreen/lang/en.js'
         ],
         css: {
-            all: ['DGFullScreen/skin/{skin}/css/DGFullScreen.css'],
-            ie: ['DGFullScreen/skin/{skin}/css/DGFullScreen.ie.css']
+            all: ['src/DGFullScreen/skin/{skin}/css/DGFullScreen.css'],
+            ie: ['src/DGFullScreen/skin/{skin}/css/DGFullScreen.ie.css']
         },
         deps: ['DGCore', 'DGLocale', 'DGRoundControl']
     },
 
     DGTileLayer: {
         desc: '2GIS tile layer module',
-        src: ['DGTileLayer/src/DGTileLayer.js'],
+        js: ['src/DGTileLayer/src/DGTileLayer.js'],
         deps: ['DGCore']
     },
 
     DGProjectDetector: {
         desc: '2GIS project detector module',
-        src: ['DGProjectDetector/src/DGProjectDetector.js'],
+        js: ['src/DGProjectDetector/src/DGProjectDetector.js'],
         deps: ['DGCore', 'DGAjax']
     },
 
     DGMeta: {
         desc: '2GIS POI & buildings data support module',
-        src: [
-            'DGMeta/src/DGMeta.js',
-            'DGMeta/src/storage/Storage.js',
-            'DGMeta/src/storage/PoiStorage.js',
-            'DGMeta/src/storage/BuildingStorage.js',
-            'DGMeta/src/StorageHost.js',
-            'DGMeta/src/PolyUtilContains.js'
+        js: [
+            'src/DGMeta/src/DGMeta.js',
+            'src/DGMeta/src/storage/Storage.js',
+            'src/DGMeta/src/storage/PoiStorage.js',
+            'src/DGMeta/src/storage/BuildingStorage.js',
+            'src/DGMeta/src/StorageHost.js',
+            'src/DGMeta/src/PolyUtilContains.js'
         ],
         deps: ['DGAjax', 'DGCore', 'DGTileLayer', 'DGWkt']
     },
 
     DGPoi: {
         desc: '2GIS POI module',
-        src: ['DGPoi/src/DGPoi.js'],
+        js: ['src/DGPoi/src/DGPoi.js'],
         deps: ['DGMeta', 'DGLabel']
     },
 
     DGBuildings: {
         desc: '2GIS buildings module',
-        src: ['DGBuildings/src/DGBuildings.js'],
+        js: ['src/DGBuildings/src/DGBuildings.js'],
         deps: ['DGMeta']
     },
 
@@ -219,59 +219,59 @@ var deps = {
         desc: '2GIS Geoclicker',
         css: {
             all: [
-                'DGGeoclicker/skin/{skin}/css/DGGeoclicker.css',
-                'DGGeoclicker/skin/{skin}/css/DGFirmCard.css',
-                'DGGeoclicker/skin/{skin}/css/DGFirmCardSkinSetup.css'
+                'src/DGGeoclicker/skin/{skin}/css/DGGeoclicker.css',
+                'src/DGGeoclicker/skin/{skin}/css/DGFirmCard.css',
+                'src/DGGeoclicker/skin/{skin}/css/DGFirmCardSkinSetup.css'
             ],
-            ie: ['DGGeoclicker/skin/{skin}/css/DGGeoclicker.ie.css']
+            ie: ['src/DGGeoclicker/skin/{skin}/css/DGGeoclicker.ie.css']
         },
-        src: [
-            'DGGeoclicker/src/DGGeoclicker.js',
-            'DGGeoclicker/src/ClampHelper.js',
-            'DGGeoclicker/src/provider/Provider.js',
-            'DGGeoclicker/src/provider/CatalogApi.js',
-            'DGGeoclicker/src/handler/Handler.js',
-            'DGGeoclicker/src/handler/Default.js',
-            'DGGeoclicker/src/handler/CityArea.js',
-            'DGGeoclicker/src/handler/House.js',
-            'DGGeoclicker/src/handler/House.View.js',
-            'DGGeoclicker/src/handler/Sight.js',
-            'DGGeoclicker/src/View.js',
-            'DGGeoclicker/src/Controller.js',
-            'DGGeoclicker/lang/it.js',
-            'DGGeoclicker/lang/ru.js',
-            'DGGeoclicker/lang/en.js',
-            'DGGeoclicker/lang/cs.js',
+        js: [
+            'src/DGGeoclicker/src/DGGeoclicker.js',
+            'src/DGGeoclicker/src/ClampHelper.js',
+            'src/DGGeoclicker/src/provider/Provider.js',
+            'src/DGGeoclicker/src/provider/CatalogApi.js',
+            'src/DGGeoclicker/src/handler/Handler.js',
+            'src/DGGeoclicker/src/handler/Default.js',
+            'src/DGGeoclicker/src/handler/CityArea.js',
+            'src/DGGeoclicker/src/handler/House.js',
+            'src/DGGeoclicker/src/handler/House.View.js',
+            'src/DGGeoclicker/src/handler/Sight.js',
+            'src/DGGeoclicker/src/View.js',
+            'src/DGGeoclicker/src/Controller.js',
+            'src/DGGeoclicker/lang/it.js',
+            'src/DGGeoclicker/lang/ru.js',
+            'src/DGGeoclicker/lang/en.js',
+            'src/DGGeoclicker/lang/cs.js',
 
-            '../vendors/firmcard/src/FirmCard.js',
-            '../vendors/firmcard/src/FirmCard.DataHelper.js',
-            '../vendors/firmcard/src/FirmList.js',
-            '../vendors/firmcard/src/vendors/underscore1.5.1.js',
-            '../vendors/firmcard/src/vendors/momentjs/moment.min.js',
-            '../vendors/firmcard/src/vendors/momentjs/lang/moment.ru.js',
-            '../vendors/firmcard/src/vendors/momentjs/lang/moment.cs.js',
-            '../vendors/firmcard/src/vendors/momentjs/lang/moment.it.js',
-            '../vendors/firmcard/src/Schedule.js',
-            '../vendors/firmcard/src/Dictionary.js'
+            '/vendors/firmcard/src/FirmCard.js',
+            '/vendors/firmcard/src/FirmCard.DataHelper.js',
+            '/vendors/firmcard/src/FirmList.js',
+            '/vendors/firmcard/src/vendors/underscore1.5.1.js',
+            '/vendors/firmcard/src/vendors/momentjs/moment.min.js',
+            '/vendors/firmcard/src/vendors/momentjs/lang/moment.ru.js',
+            '/vendors/firmcard/src/vendors/momentjs/lang/moment.cs.js',
+            '/vendors/firmcard/src/vendors/momentjs/lang/moment.it.js',
+            '/vendors/firmcard/src/Schedule.js',
+            '/vendors/firmcard/src/Dictionary.js'
         ],
         deps: ['DGAjax', 'DGWhen', 'DGCore', 'DGTemplate', 'DGLocale', 'DGPoi', 'DGEntrance', 'DGProjectDetector']
     },
 
     DGTemplate: {
         desc: '2GIS Template',
-        src: ['DGTemplate/src/DGTemplate.js']
+        js: ['src/DGTemplate/src/DGTemplate.js']
     },
 
     DGEntrance: {
         desc: '2GIS Entrances',
-        src: [
-            'DGEntrance/src/DGEntrance.js',
-            'DGEntrance/src/PathAnimation.js',
-            'DGEntrance/src/Arrow.js',
-            'DGEntrance/src/ArrowSvg.js',
-            'DGEntrance/src/ArrowVml.js',
-            'DGEntrance/src/ArrowSvgAnimationOptions.js',
-            'DGEntrance/src/EventHandler.js'
+        js: [
+            'src/DGEntrance/src/DGEntrance.js',
+            'src/DGEntrance/src/PathAnimation.js',
+            'src/DGEntrance/src/Arrow.js',
+            'src/DGEntrance/src/ArrowSvg.js',
+            'src/DGEntrance/src/ArrowVml.js',
+            'src/DGEntrance/src/ArrowSvgAnimationOptions.js',
+            'src/DGEntrance/src/EventHandler.js'
         ],
         deps: ['DGCore', 'DGWkt', 'DGProjectDetector']
     },
@@ -279,10 +279,10 @@ var deps = {
 
     DGRoundControl: {
         desc: 'Control helper',
-        src: ['DGRoundControl/src/DGRoundControl.js'],
+        js: ['src/DGRoundControl/src/DGRoundControl.js'],
         css: {
-            all: ['DGRoundControl/skin/{skin}/css/DGRoundControl.css'],
-            ie: ['DGRoundControl/skin/{skin}/css/DGRoundControl.ie.css']
+            all: ['src/DGRoundControl/skin/{skin}/css/DGRoundControl.css'],
+            ie: ['src/DGRoundControl/skin/{skin}/css/DGRoundControl.ie.css']
         },
         deps: ['DGCore', 'DGLocale']
     }
--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,61 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var test = require('gulp-test');
+//var deps = require('gulp-deps');
 var minifyCSS = require('gulp-minify-css');
-var deps = require('./build/deps.js');
+var config = require('./build/deps.js').deps;
+var runSequence = require('run-sequence');
+var clean = require('gulp-clean');
 
-gulp.task('buildjs', function () {
-    //console.log(deps);
-    return gulp.src('./src/**/src/**/*.js')
-            //.pipe(concat('all.js'))
-             .pipe(test())
-            /*.pipe(rename('min.js'))
-            .pipe(uglify())*/
-            .pipe(gulp.dest('./dist'));
+gulp.task('build-deps', function () {
+    Object.keys(config).forEach(function (module) {
+        //console.log(module, config[module].js);
+        var scripts = config[module].js,
+            styles = config[module].css ? config[module].css.all : undefined;
+
+        console.log(styles);
+
+        if (scripts)  {
+            gulp.src(scripts)
+                .pipe(concat(module + '.js'))
+                .pipe(gulp.dest('./dist/' + module + '/js/'))
+                .pipe(rename(module + '.min.js'))
+                .pipe(uglify())
+                .pipe(gulp.dest('./dist/' + module + '/js/'));
+        }
+
+        if (styles) {
+            gulp.src(styles)
+                .pipe(concat(module + '.css'))
+                .pipe(gulp.dest('./dist/' + module + '/css/'))
+                .pipe(rename(module + '.min.css'))
+                .pipe(minifyCSS())
+                .pipe(gulp.dest('./dist/' + module + '/css/'));
+        }
+    });
 });
 
-gulp.task('buildcss', function () {
-    return gulp.src('./src/**/skin/**/*.css')
+/*gulp.task('build-scripts', function (module, files) {
+    return gulp.src(files)
+            .pipe(concat(module + '.js'))
+            .pipe(gulp.dest('./dist/' + module + '/js/'))
+            .pipe(rename(module + '.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('./dist/' + module + '/js/'));
+});
+
+gulp.task('build-styles', function (module, files) {
             //.pipe(concat('all.css'))
             .pipe(gulp.dest('./dist'))
             .pipe(rename('min.css'))
             .pipe(minifyCSS())
             .pipe(gulp.dest('./dist'));
+});*/
+
+gulp.task('build-clean', function () {
+    return gulp.src('./dist').pipe(clean());
 });
 
-gulp.task('build', ['buildjs', 'buildcss']);
+gulp.task('build', function (cb) {
+    runSequence('build-clean', 'build-deps', cb);
+});
