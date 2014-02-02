f021684477c4f1f33585139d9ca0fefec18c1704
Dmitry Lutsik <otoyto@gmail.com>
2014-02-02 14:06:55 +0200

    css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    async = require('async'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
@@ -22,8 +21,18 @@ function bldjs() {
                .pipe(cache(uglify()));
 }
 
+function bldcss() {
+    return gulp.src(deps.getCSSFiles())
+               .pipe(concat('main.css'))
+               .pipe(cache(minifyCSS()));
+}
+
 gulp.task('build-scripts', ['build-clean'], function () {
     return bldjs();
+});
+
+gulp.task('build-styles', ['build-clean'], function () {
+    return bldcss();
 });
 
 gulp.task('build-clean', function () {
@@ -31,5 +40,5 @@ gulp.task('build-clean', function () {
 });
 
 module.exports = function () {
-    return bldjs();
+    return bldcss();
 };
