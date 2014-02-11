245f11646911848b1ec04de7630caa5cca568894
Dmitry Lutsik <otoyto@gmail.com>
2014-02-11 16:53:27 +0200

    flatten fix

--- build/gulp-flatten/index.js
+++ build/gulp-flatten/index.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var fs = require('fs');
 var through2 = require('through2');
 var PluginError = require('gulp-util').PluginError;
 
@@ -8,17 +7,14 @@ module.exports = function(opts) {
   opts.newPath = opts.newPath || '';
 
   return through2.obj(function(file, enc, next) {
-    var that = this;
-
-    fs.lstat(file.path, function (err, stats) {
-      if (!stats.isFile()) return next();
-      try {
-          file.path = path.join(file.base, opts.newPath, path.basename(file.path));
-          that.push(file);
-      } catch (e) {
-        that.emit('error', new PluginError('gulp-flatten', e));
+    try {
+      if (file.stat.isFile()) {
+        file.path = path.join(file.base, opts.newPath, path.basename(file.path));
+        this.push(file);
       }
-      next();
-    });
+    } catch (e) {
+      this.emit('error', new PluginError('gulp-flatten', e));
+    }
+    next();
   });
 };
