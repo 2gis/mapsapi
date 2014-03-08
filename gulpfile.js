eb702a2515cce4d86fd983420c44566e4f066cec
Alexander Belov <p@mesquite.ru>
2014-03-08 10:18:15 +0700

    JSAPI-3378: gulp-less/less paths option accurate fix

--- gulpfile.js
+++ gulpfile.js
@@ -269,7 +269,9 @@ function bldCss(opt) {
 
     return gulp.src(lessList)
                 .pipe(tasks.header(lessPrerequirements))
-                .pipe(tasks.less())
+                .pipe(tasks.less({
+                    paths: []
+                }))
                 //.pipe(tasks.frep(config.cfgParams))
                 .pipe(tasks.cache(tasks.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')))
                 .pipe(tasks.base64({
