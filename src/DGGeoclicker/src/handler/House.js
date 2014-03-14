04970512b82aefa1ef52fc2f7f0a1d27b4473bce
Alexander Belov <p@mesquite.ru>
2014-03-14 16:42:15 +0700

    JSAPI-3386: DGGeoclicker footer buttons icons fix

--- src/DGGeoclicker/src/handler/House.js
+++ src/DGGeoclicker/src/handler/House.js
@@ -125,7 +125,7 @@ DG.Geoclicker.Handler.House = DG.Geoclicker.Handler.Default.extend({
                 data: {
                     btns: [
                         {
-                            name: 'firmList-back',
+                            name: 'firm-list-back',
                             label: this.t('back button'),
                             icon: true
                         }
@@ -220,7 +220,7 @@ DG.Geoclicker.Handler.House = DG.Geoclicker.Handler.Default.extend({
     },
 
     _initShowLess: function () {
-        var link = this._popup.findElement('.dg-popup__button_name_firmList-back');
+        var link = this._popup.findElement('.dg-popup__button_name_firm-list-back');
 
         if (link) {
             this._addEventHandler('DgShowLessClick', link, 'click', DG.bind(this._showHousePopup, this));
