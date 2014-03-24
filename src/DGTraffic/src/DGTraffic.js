676ae6ccc4efa531eab3f4077cb49c2856c7b764
Dmitry Lutsik <otoyto@gmail.com>
2014-03-24 16:43:28 +0200

    fix empty image error

--- src/DGTraffic/src/DGTraffic.js
+++ src/DGTraffic/src/DGTraffic.js
@@ -1,10 +1,11 @@
 DG.TrafficLayer = DG.TileLayer.extend({
 
     options: {
+        errorTileUrl: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
         subdomains: '012345679',
         period: 0
     },
-    
+
     statics: {
         tileUrl: '__TRAFFIC_TILE_SERVER__',
         metaUrl: '__TRAFFIC_META_SERVER__'
@@ -19,7 +20,7 @@ DG.TrafficLayer = DG.TileLayer.extend({
 
         project ? this._setProjectOptions(project) : this._setNullOptions();
 
-        this._handler = new DG.TrafficHandler(map);
+        this._handler = new DG.Traffic(map);
         this._handler.enable();
 
         DG.TileLayer.prototype.onAdd.call(this);
