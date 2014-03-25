69e366debc34319a678826210523f6cfbb38a64b
Dmitry Lutsik <otoyto@gmail.com>
2014-03-25 12:11:06 +0200

    handle undefined traffic response

--- src/DGMeta/src/StorageHost.js
+++ src/DGMeta/src/StorageHost.js
@@ -45,7 +45,7 @@ DG.Meta.Host = DG.Class.extend({
             return {
                 poi: self._poiStorage.addDataToTile(tileId, result.poi),
                 buildings: self._buildingStorage.addDataToTile(tileId, result.buildings),
-                traffic: self._trafficStorage.addDataToTile(tileId, data[1])
+                traffic: self._trafficStorage.addDataToTile(tileId, data[1] || {})
             };
         }, function () {
             return false;
