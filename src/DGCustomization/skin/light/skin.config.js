abfc73f171ab07ecc2284a9043c759efed31aab5
y.voloshchuk <wenqeer@gmail.com>
2014-03-31 13:36:19 +0300

    JSAPI-3545 marker fix

--- src/DGCustomization/skin/light/skin.config.js
+++ src/DGCustomization/skin/light/skin.config.js
@@ -5,5 +5,11 @@ DG.configTheme = {
             x: 0,
             y: 5
         }
+    },
+
+    markersData: {
+        iconSize: [22, 34],
+        className: 'dg-customization__marker dg-customization__marker_type_mushroom',
+        iconAnchor: [10, 28]
     }
 };
