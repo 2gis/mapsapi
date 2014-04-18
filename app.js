631e0106bd4ed92a6dce34eebf1b5e65da1f7cfa
Alexander Belov <p@mesquite.ru>
2014-04-18 14:17:06 +0700

    JSAPI-3378: The last replacement IE → IE8

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/2.0/js', getParams, function (req, res) {
 app.get('/2.0/css', getParams, function (req, res) {
     var cssOptions = {
             includeModernBrowsers: true,
-            includeIE: !!req.query.ie,
+            includeIE8: !!req.query.ie,
             useSprites: !!req.query.sprite
         },
         cssStream = gulp.getCSS(cssOptions);
