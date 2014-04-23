3940cd71f12df264af359fae0b3fa3173ec11a57
Anton Borodatov <anton.borodatov@gmail.com>
2014-04-23 19:24:25 +0300

    {n} translation fix

--- src/DGGeoclicker/lang/cs.js
+++ src/DGGeoclicker/lang/cs.js
@@ -11,5 +11,5 @@ DG.Geoclicker.Handler.Default.Dictionary.cs = DG.extend({
     show_organization_in_building : ['Celkem {n} organizace', 'Celkem {n} organizace', 'Celkem {n} organizací'],
     back_button : 'Zpět',
     show_more_about_sight : 'Více',
-    n_floors : ['{n} patro', '{n} patra', 'pater']
+    n_floors : ['{n} patro', '{n} patra', '{n} pater']
 }, DG.Dictionary.cs);
