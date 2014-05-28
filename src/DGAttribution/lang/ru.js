d503d479fea51e66f337d1c08cf96978078669dd
v.zagorodniy <zag.valentin@gmail.com>
2014-05-28 12:01:28 +0300

    cleaning unused keys in DGAttribution

--- src/DGAttribution/lang/ru.js
+++ src/DGAttribution/lang/ru.js
@@ -3,7 +3,5 @@ DG.Control.Attribution.Dictionary.ru = {
 	copyright_apilink : 'http://api.2gis.ru/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
 	copyright_license : 'http://help.2gis.ru/licensing-agreement/',
 	license_agreement : 'Лицензионное соглашение',
-	API_2GIS : 'Работает на API 2ГИС',
-	TWO_GIS: 'ДубльГИС',
-	PTC: 'ООО'
+	API_2GIS : 'Работает на API 2ГИС'
 };
--- src/DGAttribution/lang/sp.js
+++ src/DGAttribution/lang/sp.js
@@ -3,7 +3,5 @@ DG.Control.Attribution.Dictionary.sp = {
 	copyright_apilink : 'http://santiago.2gis.cl/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
 	copyright_license : 'http://law.2gis.cl/licensing-agreement/',
 	license_agreement : 'Acuerdo de licencia',
-	API_2GIS: '2GIS API',
-	TWO_GIS: 'DublGIS',
-	PTC: 'S.L.'
+	API_2GIS: '2GIS API'
 };
