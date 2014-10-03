d758af9be13fd8975d50ce3b983abd6f42742f71
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2014-10-03 17:50:33 +0700

    add tests for DG.TileLayer

---
+++ src/DGTileLayer/test/DGTileLayerSpec.js
@@ -0,0 +1,32 @@
+describe('DG.TileLayer', function() {
+    var mapContainer = document.createElement('div'),
+        map = new DG.Map(mapContainer, {
+            center: [54.980206086231, 82.898068362003],
+            zoom: 15
+        });
+
+    after(function() {
+        mapContainer = map = null;
+    });
+
+    describe('check init', function() {
+        it('should tile url contain \'2gis\'', function() {
+            var img = map.getPane('mapPane').getElementsByTagName('img');
+
+            expect(img.length > 0).to.be.ok();
+
+            expect(img[0].getAttribute('src')).contain('2gis');
+        });
+
+        it('should be map.baseLayer', function() {
+            expect(map.baseLayer).to.be.a('object');
+        });
+    });
+
+    describe('#onRemove', function() {
+        it('should projectDetector is disable', function() {
+            map.baseLayer.remove();
+            expect(map.projectDetector.enabled()).not.be.ok();
+        })
+    });
+});
