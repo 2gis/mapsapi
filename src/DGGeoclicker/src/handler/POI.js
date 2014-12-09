549c7c2e23eecc8e3c97b7a3a69f453c07b42d2a
Alex Fedosov <alex@fedosov.me>
2014-12-09 12:24:09 +0600

    fix galleries poi callouts #65

--- src/DGGeoclicker/src/handler/POI.js
+++ src/DGGeoclicker/src/handler/POI.js
@@ -5,8 +5,16 @@ DG.Geoclicker.Handler.Poi = DG.Geoclicker.Handler.House.extend({
             return false;
         }
 
-        DG.Geoclicker.Handler.House.prototype.handle.call(this, results);
+        var houseHandlerResult =
+            DG.Geoclicker.Handler.House.prototype.handle.call(this, results);
 
+        // If the POI refers to a building (e.g. galleries in Santiago), simply
+        // show a building callout
+        if (results.poi.reference.type === 'building') {
+            return houseHandlerResult;
+        }
+
+        // Otherwise, show a firm callout
         this._firmCardObject = this._fillFirmCardObject(results.poi.reference.id);
         return Promise.resolve(this._firmCardObject);
     }
