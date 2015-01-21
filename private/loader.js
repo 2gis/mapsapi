33a478c59e00ca479e869c03be1eb31e1194908c
Alex Fedosov <alex@fedosov.me>
2015-01-21 18:44:30 +0600

    release 2.0.32

--- private/loader.js
+++ private/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v2.0.31';
+    var version = 'v2.0.32';
 
     var url = (function () {
         var scripts = document.getElementsByTagName('script');
