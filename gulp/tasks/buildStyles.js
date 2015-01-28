efeb400d6302da44e35c578fb88357000e86df33
Dmitry Vibe <riim@yandex.ru>
2015-01-28 16:22:24 +0600

    remove gulp.start

--- gulp/tasks/buildStyles.js
+++ gulp/tasks/buildStyles.js
@@ -1,10 +1,13 @@
+
+var runSequence = require('run-sequence');
 var gulp = require('gulp');
 
 var destCSS = require('../util/destCSS');
 
-gulp.task('buildStyles', ['buildClean', 'collectImagesStats', 'generateSprites'], function (cb) {
-    destCSS({}, function() {
-    	gulp.start('lintCSS');
-    	cb();
-    });
+gulp.task('buildStyles-notLint', ['buildClean', 'collectImagesStats', 'generateSprites'], function (cb) {
+    destCSS({}, cb);
+});
+
+gulp.task('buildStyles', function (cb) {
+    runSequence('buildStyles-notLint', 'lintCSS', cb);
 });
