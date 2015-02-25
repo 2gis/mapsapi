86d5f7fa1a4f480155f01e31058c1b491cc7f280
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-02-25 16:29:04 +0600

    build images only for modules

--- gulp/tasks/copyRaster.js
+++ gulp/tasks/copyRaster.js
@@ -1,15 +1,18 @@
 var flatten = require('gulp-flatten');
 var rename = require('gulp-rename');
+var util = require('gulp-util');
 var gulp = require('gulp');
 var path = require('path');
 
 var error = require('../util/error');
+var config = require('../../build/config');
+var deps = require('../../build/gulp-deps')(config);
 
 gulp.task('copyRaster', function () {
-    return gulp.src(['src/**/img/**/*.{png,gif,jpg,jpeg}'])
+    return gulp.src(deps.getImgGlob(util.env))
         .pipe(error.handle())
         .pipe(rename(function (p) {
-            p.dirname = p.dirname.split(path.sep)[2];
+            p.dirname = p.dirname.split(path.sep)[1];
         }))
         .pipe(gulp.dest('build/tmp/img'))
         .pipe(flatten())
--- gulp/tasks/rasterAndCopySVG.js
+++ gulp/tasks/rasterAndCopySVG.js
@@ -3,27 +3,30 @@ var rename = require('gulp-rename');
 var rsvg = require('gulp-rsvg');
 var gulp = require('gulp');
 var path = require('path');
+var util = require('util');
 
 var error = require('../util/error');
+var config = require('../../build/config');
+var deps = require('../../build/gulp-deps')(config);
 
 gulp.task('rasterAndCopySVG', function (cb) {
-    gulp.src('src/**/img/**/*.svg')
+    gulp.src(deps.getSVGGlob(util.env))
         .pipe(error.handle())
         .pipe(rsvg())
         .pipe(rename(function (p) {
-            p.dirname = p.dirname.split(path.sep)[2];
+            p.dirname = p.dirname.split(path.sep)[1];
         }))
         .pipe(gulp.dest('build/tmp/img'))
         .pipe(flatten())
         .pipe(gulp.dest('build/tmp/img_all'))
         .pipe(gulp.dest('public/img'))
         .on('end', function () {
-            gulp.src('src/**/img/**/*.svg')
+            gulp.src(deps.getSVGGlob(util.env))
                 .pipe(error.handle())
                 .pipe(rsvg({scale: 2}))
                 .pipe(rename(function (p) {
                     p.extname = '@2x.png';
-                    p.dirname = p.dirname.split(path.sep)[2];
+                    p.dirname = p.dirname.split(path.sep)[1];
                 }))
                 .pipe(gulp.dest('build/tmp/img'))
                 .pipe(flatten())
