16680a93014dc8130255ad10b23921c6bd8245d7
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-02-27 15:19:18 +0600

    return gulp-cache fix

--- gulp/util/fixFileCache.js
+++ gulp/util/fixFileCache.js
@@ -1,4 +1,3 @@
-
 var path = require('path');
 var mkdirp = require('mkdirp');
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var requireDir = require('require-dir');
+var cache = require('gulp-cache');
 
 requireDir('./gulp/tasks', { recurse: true });
+
+// fix file cache due to buggy realization
+// https://github.com/2gis/mapsapi/pull/38
+require('./gulp/util/fixFileCache')(cache.fileCache, './build/cache');
 
 module.exports = {
     getJS: require('./gulp/util/buildJS'),
