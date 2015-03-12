e3df5583effbdd4f28c12907846977439f8085c8
Mstislav Zhivodkov <m.zhivodkov@2Gis.ru>
2015-03-12 17:31:46 +0600

    refactoring DGZoom

--- src/DGZoomControl/src/DGZoomControl.js
+++ src/DGZoomControl/src/DGZoomControl.js
@@ -2,7 +2,6 @@ DG.Control.Zoom.include(DG.Locale);
 DG.Control.Zoom.Dictionary = {};
 
 DG.Control.Zoom.include({
-    // TODO: think about pull request to leaflet with zoom control button's titles as options
     onAdd: function (map) {
         var zoomName = 'dg-zoom',
             buttonTemplate = '<div class="dg-control-round__icon ' + zoomName + '__control ' + zoomName + '__button ' + zoomName + '__button_type_{type}"></div>',
@@ -13,16 +12,15 @@ DG.Control.Zoom.include({
         this._zoomInButton = this._createButton(DG.Util.template(buttonTemplate, {type : 'in'}), this.t('zoom_in'), 'dg-control-round ' + zoomName + '__in', container, this._zoomIn, this);
         this._zoomOutButton = this._createButton(DG.Util.template(buttonTemplate, {type : 'out'}), this.t('zoom_out'), 'dg-control-round ' + zoomName + '__out', container, this._zoomOut, this);
 
-        this._eventListeners = {};
-        this._eventListeners.zoomend = this._eventListeners.zoomlevelschange = this._updateDisabled;
         this._updateDisabled();
-        map.on(this._eventListeners, this);
+        map.on('zoomend zoomlevelschange', this._updateDisabled, this);
 
         return container;
     },
 
     _originalCreateButton: DG.Control.Zoom.prototype._createButton,
 
+    // set active state control on mobile devices
     _createButton: function () {
         var args = Array.prototype.slice.call(arguments);
         var link = this._originalCreateButton.apply(this, args);
@@ -42,10 +40,6 @@ DG.Control.Zoom.include({
             });
 
         return link;
-    },
-
-    onRemove: function (map) {
-        map.off(this._eventListeners, this);
     },
 
     _renderTranslation: function () {
