335c3102582f1b5a3f1798df341e351c93fc2632
andymost <andreymostcase@gmail.com>
2015-06-11 14:29:01 +0600

    Modify test template render

--- functional-tests/template_render.py
+++ functional-tests/template_render.py
@@ -4,8 +4,8 @@ from templates.content import pages
 import codecs
 import os
 
-if not os.path.exists('../public/pages'):
-    os.mkdir('../public/pages')
+if not os.path.exists('../dist/pages'):
+    os.mkdir('../dist/pages')
 
 base = open('templates/base.html', 'r')
 template = Template(base.read())
@@ -14,6 +14,6 @@ base.close()
 parts = pages()
 
 for page in parts:
-    out = codecs.open("../public/pages/%s.html" % parts[page]['title'], 'w', 'utf-8')
+    out = codecs.open("../dist/pages/%s.html" % parts[page]['title'], 'w', 'utf-8')
     out.write(template.render(parts[page]))
     out.close()
