7d63643893f081d716c4bdccfec591b394f8dd31
andymost <andreymostcase@gmail.com>
2015-06-26 17:27:20 +0600

    Release 2.1.1

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v2.1.0';
+    var version = 'v2.1.1';
 
     var url = (function () {
         var scripts = document.getElementsByTagName('script');
