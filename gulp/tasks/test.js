c0ce35255da9f77a8043636e14a5624e397afac3
vincent <cherebedov.sergey@gmail.com>
2016-02-09 20:53:10 +0600

    Was added splitter of tests. Need review.

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,6 +2,7 @@ var Server = require('karma').Server;
 var util = require('gulp-util');
 var gulp = require('gulp');
 var path = require('path');
+var glob = require('glob');
 var _ = require('lodash');
 
 var error = require('../util/error');
@@ -11,10 +12,15 @@ var deps = require('../deps')(config);
 
 var isTestDebug = util.env.d || util.env.debug;
 var testRequirements = isTestDebug ? [] : ['buildTest'];
+var itemInChunk = util.env['items-in-chunk'] || 5;
+
+// TODO: Remove after update lodash to v3.10.1 or higher.
+_.chunk = require('../../node_modules/karma/node_modules/lodash/array/chunk.js');
 
 gulp.task('test', testRequirements, function (done) {
     var cliOptions = _.cloneDeep(util.env);
     var modulesToTest = [];
+    var currentChunk = 0;
 
     var sourcesList = deps.getJSFiles({source: 'testSource'}).concat([
         'gulp/tmp/testJS/config.js',
@@ -34,27 +40,42 @@ gulp.task('test', testRequirements, function (done) {
         modulesToTest = cliOptions.module.split(',');
     }
 
+    var modulesToTestSourceList = [];
+
     if (modulesToTest.length) {
         modulesToTest.forEach(function (moduleName) {
-            sourcesList.push('src/' + moduleName + '/test/*Spec.js');
+            modulesToTestSourceList = modulesToTestSourceList.concat(glob.sync('src/' + moduleName + '/test/*Spec.js'));
         });
     } else {
-        sourcesList.push('src/**/test/*Spec.js');
+        modulesToTestSourceList = modulesToTestSourceList.concat(glob.sync('src/**/test/*Spec.js'));
     }
 
-    sourcesList.push('node_modules/leaflet/spec/suites/SpecHelper.js');
-    sourcesList.push('node_modules/leaflet/spec/suites/**/*Spec.js');
+    console.log("ITEMS IN CHUNK: " + itemInChunk + ". Use --items-in-chunk for set this.");
 
-    new Server({
-        files: sourcesList,
-        configFile: path.join(__dirname, '../../test/karma.conf.js'),
-        browsers: test.getBrowsers(),
-        reporters: test.getReporters(isTestDebug),
-        junitReporter: test.getJunitReporter(),
-        action: 'run',
-        preprocessors: {
-            'gulp/tmp/testJS/src/**/*.js': ['coverage']
-        },
-        singleRun: true
-    }, done).start();
+    var splittedModulesToTestSourceList = _.chunk(modulesToTestSourceList, itemInChunk);
+    var numberOfChunks = splittedModulesToTestSourceList.length;
+
+    console.log("NUMBER OF CHUNKS: " + numberOfChunks);
+
+    var startServer = function(){
+        console.log('\nCHUNK #' + currentChunk.toString());
+
+        var localeSourceList = sourcesList.concat(splittedModulesToTestSourceList[currentChunk++]);
+        localeSourceList.push('node_modules/leaflet/spec/suites/SpecHelper.js');
+        localeSourceList.push('node_modules/leaflet/spec/suites/**/*Spec.js');
+
+        new Server({
+            files: localeSourceList,
+            configFile: path.join(__dirname, '../../test/karma.conf.js'),
+            browsers: test.getBrowsers(),
+            reporters: test.getReporters(isTestDebug),
+            junitReporter: test.getJunitReporter(),
+            action: 'run',
+            preprocessors: {
+                'gulp/tmp/testJS/src/**/*.js': ['coverage']
+            },
+            singleRun: true
+        }, currentChunk == numberOfChunks ? done : startServer).start();
+    };
+    startServer();
 });
