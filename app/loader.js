7b51f10706da846b35a296549ff4a4395c577aab
andymost <andreymostcase@gmail.com>
2016-04-29 14:04:13 +0600

    Release 2.2.3

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v2.2.2';
+    var version = 'v2.2.3';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
