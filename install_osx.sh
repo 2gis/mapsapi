ca729d841bac69d5f55523f58744dd949c0c56ae
Artyom Egorov <mail@egoroof.ru>
2017-02-06 15:49:14 +0700

    Don't install NPM manually
    It comes with Nodejs

--- install_osx.sh
+++ install_osx.sh
@@ -3,7 +3,6 @@
 echo 'Preparing to install Node.js & mapsapi'
 
 node -v || brew install node
-sudo npm install -g npm@2.x
 npm install
 sudo npm install -g gulp
 
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,7 +7,6 @@ sudo apt-get install -y python-software-properties make gcc g++
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 node -v || sudo apt-get install -y nodejs
-sudo npm install -g npm@2.x
 npm install
 sudo npm install -g gulp
 
