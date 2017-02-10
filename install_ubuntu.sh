c9cd6324640f1861462a81a9f4aad3020b8582cc
Artyom Egorov <mail@egoroof.ru>
2017-02-10 12:14:14 +0700

    Remove build tools from ubuntu installer

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,7 +3,6 @@
 echo 'Preparing to install Node.js & mapsapi'
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties make gcc g++
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 node -v || sudo apt-get install -y nodejs
