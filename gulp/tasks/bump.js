70c136a450d6eeb04196ae7f51fac426d312dfd7
Artyom Egorov <mail@egoroof.ru>
2017-04-28 19:21:41 +0700

    Drop gulp release

--- gulp/tasks/bump.js
+++
@@ -1,9 +0,0 @@
-var bump = require('gulp-bump');
-var util = require('gulp-util');
-var gulp = require('gulp');
-
-gulp.task('bump', function() {
-    return gulp.src('package.json')
-        .pipe(bump(util.env))
-        .pipe(gulp.dest('./'));
-});
--- gulp/tasks/bumpLoader.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-var config = require('../../app/config');
-
-gulp.task('bumpLoader', ['bump'], function(done) {
-    config.updateLoaderVersion(done);
-});
