d4d377b4f7e1ab1d5baafc501e0818cc828da648
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2017-06-16 14:02:16 +0700

    Leaflet custom build (#372)

--- gulp/tasks/buildLeaflet.js
+++ gulp/tasks/buildLeaflet.js
@@ -1,11 +1,57 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
+var util = require('gulp-util');
+var path = require('path');
 
 var config = require('../../app/config.js');
 var deps = require('../deps')(config);
 
-gulp.task('buildLeaflet', function() {
-    return gulp.src(deps.getJSFiles({source: 'leaflet'}))
+function getLeafletFiles(compsBase32) {
+    var memo = {},
+        deps = require('leaflet/build/deps.js').deps,
+        comps;
+
+    if (compsBase32) {
+        comps = parseInt(compsBase32, 32).toString(2).split('');
+        console.log('Managing dependencies...');
+    }
+
+    function addFiles(srcs) {
+        for (var j = 0, len = srcs.length; j < len; j++) {
+            memo[srcs[j]] = true;
+        }
+    }
+
+    for (var i in deps) {
+        if (comps) {
+            if (parseInt(comps.pop(), 2) === 1) {
+                console.log(' * ' + i);
+                addFiles(deps[i].src);
+            } else {
+                console.log('   ' + i);
+            }
+        } else {
+            addFiles(deps[i].src);
+        }
+    }
+
+    console.log('\n');
+
+    var files = [];
+
+    for (var src in memo) {
+        files.push('src/' + src);
+    }
+
+    return files;
+}
+
+// leafletCustomBuild parameter for set id of leaflet build. See more in leaflet/build/build.html
+gulp.task('buildLeaflet', function () {
+    var leafletCustomBuild = util.env['leaflet-custom-build'];
+    return (leafletCustomBuild ? gulp.src(getLeafletFiles(leafletCustomBuild).map(function (file) {
+        return path.resolve(path.join(__dirname, '../../node_modules/leaflet/', file))
+    })) : gulp.src(deps.getJSFiles({source: 'leaflet'})))
         .pipe(concat('leaflet-src.js'))
         .pipe(gulp.dest('node_modules/leaflet/dist/'));
 });
