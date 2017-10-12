5beefdfa48310bbaba4221c121b09cf57ed3db38
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-10-12 17:31:50 +0700

    Fix retina traffic logic (#448)

--- src/DGTraffic/src/DGTraffic.js
+++ src/DGTraffic/src/DGTraffic.js
@@ -18,7 +18,7 @@ DG.Traffic = DG.TileLayer.extend({
             errorTileUrl: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
             subdomains: '012345679',
             maxNativeZoom: 18,
-            detectRetina: true,
+            detectRetina: DG.config.detectRetina,
             minZoom: DG.config.trafficLayerMinZoom
         };
 
