76f049262c691de78e03cf64fbdd0beb9053030a
Vasiliy Korostelev <v.korostelev@2gis.ru>
2018-04-11 16:46:35 +0700

    Release 3.4.6

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v3.4.5';
+    var version = 'v3.4.6';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
