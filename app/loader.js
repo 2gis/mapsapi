380c4ad0acae3c6f04d1a1ab20b7f1420145f6c0
Vasiliy Korostelev <v.korostelev@2gis.ru>
2018-05-30 11:32:26 +0700

    Release 3.4.10

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v3.4.9';
+    var version = 'v3.4.10';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
