8908cd529144a9a816d774fd6ca50ee89fac120e
Mstislav Zhivodkov <stevemyz@gmail.com>
2020-02-03 16:35:59 +0700

    Release 3.5.13

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v3.5.12';
+    var version = 'v3.5.13';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
