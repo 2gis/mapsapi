2b7dd02fb1b7658e91aaaf5014d848dc7e1978d4
Mstislav Zhivodkov <stevemyz@gmail.com>
2021-03-12 15:13:49 +0700

    Release 3.6.4 (#546)

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v3.6.3';
+    var version = 'v3.6.4';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
