c9953d6648633b3c32a9dbd862e9699e41e57516
Andrey Kuznetsov <mail@kuznetsov.it>
2021-12-16 13:59:29 +0600

    TILES-3943 Add route case (#556)
    * TILES-3943 Add route case
    
    * fix UML label
    
    * Fix review
    
    * fix remove project link
    
    * fix review
    
    * Update src/DGAttribution/src/DGAttribution.js
    
    Co-authored-by: itanka9 <itano@yandex.ru>
    
    * fix update
    
    * Update src/DGAttribution/src/DGAttribution.js
    
    Co-authored-by: Mstislav Zhivodkov <stevemyz@gmail.com>
    
    * fix
    
    * fix init link
    
    Co-authored-by: Andrey Kuznecov <a.kuznecov@2gis.ru>
    Co-authored-by: itanka9 <itano@yandex.ru>
    Co-authored-by: Mstislav Zhivodkov <stevemyz@gmail.com>

--- src/DGAttribution/src/DGAttribution.js
+++ src/DGAttribution/src/DGAttribution.js
@@ -12,36 +12,42 @@ DG.Control.Attribution.include({
                 copyright_logo: 'http://info.2gis.ru/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://api.2gis.ru/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_license: 'http://law.2gis.ru/api-rules/',
+                open_link: 'http://2gis.ru',
             },
 
             it: {
                 copyright_logo: 'http://2gis.it/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://2gis.it/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
-                copyright_license: 'http://law.2gis.it/licensing-agreement/'
+                copyright_license: 'http://law.2gis.it/licensing-agreement/',
+                open_link: 'http://2gis.ru',
             },
 
             cz: {
                 copyright_logo: 'http://praha.2gis.cz/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://praha.2gis.cz/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
-                copyright_license: 'http://law.2gis.cz/api-rules/'
+                copyright_license: 'http://law.2gis.cz/api-rules/',
+                open_link: 'http://2gis.ru',
             },
 
             cl: {
                 copyright_logo: 'http://santiago.2gis.cl/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://santiago.2gis.cl/?utm_source=copyright&utm_medium=map&utm_campaign=partners',
-                copyright_license: 'http://law.2gis.cl/api-rules/'
+                copyright_license: 'http://law.2gis.cl/api-rules/',
+                open_link: 'http://2gis.ru',
             },
 
             cy: {
                 copyright_logo: 'http://info.2gis.com.cy/lemesos?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://info.2gis.com.cy/lemesos?utm_source=copyright&utm_medium=map&utm_campaign=partners',
-                copyright_license: 'http://law.2gis.com.cy/api-rules/'
+                copyright_license: 'http://law.2gis.com.cy/api-rules/',
+                open_link: 'http://2gis.ru',
             },
 
             ae: {
                 copyright_logo: 'http://info.2gis.ae/dubai?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_apilink: 'http://info.2gis.ae/dubai?utm_source=copyright&utm_medium=map&utm_campaign=partners',
                 copyright_license: 'http://law.2gis.ae/api-rules/',
+                open_link: 'http://2gis.ae',
             }
         };
         /* eslint-enable camelcase */
@@ -51,12 +57,51 @@ DG.Control.Attribution.include({
         return dictionary[countryCode][linkType];
     },
 
+    _markerToRoute: undefined,
+    _markers: [],
+
+    _checkMarkerLayers: function() {
+        this._markerToRoute = this._markers.length != 1 ? undefined : this._markers[0];
+        this._update();
+    },
+
+    _mapEvents: {
+
+        layeradd: function(e) {
+            if (e.layer instanceof DG.Marker) {
+                this._markers.push(e.layer)
+                if (this._markers.length <= 2) {
+                    this._checkMarkerLayers();
+                }
+            }
+        },
+
+        layerremove: function(e) {
+            if (e.layer instanceof DG.Marker) {
+                var currentMarkers = [];
+                for (var i = 0; i < this._markers.length; i++) {
+                    if (this._markers[i] !== e.layer) {
+                        currentMarkers.push(this._markers[i])
+                    }
+                }
+                this._markers = currentMarkers;
+                if (this._markers.length <= 1) {
+                    this._checkMarkerLayers();
+                }
+            }
+        },
+
+    },
+
     onAdd: function(map) {
         if (!map._copyright) {
             map._copyright = true;
             this._first = true;
         }
         this._logotype = map.options.logotype;
+        this._open2gis = this._getLink('open_link')
+
+        this._map.on(this._mapEvents, this);
 
         map.attributionControl = this;
         this._container = DG.DomUtil.create('div', 'dg-attribution');
@@ -70,21 +115,20 @@ DG.Control.Attribution.include({
 
         this._update();
 
-        var self = this;
         DG.DomEvent
-            .on(this._container, 'touchstart', function(e) {
-                if (e.target.href === self._open2gis) {
-                    self._open2gis = self._getOpenUrl();
-                    e.target.href = self._open2gis
-                }
-            })
+            .on(this._container, 'touchstart', this._updateLink, this)
         return this._container;
+    },
+
+    _updateLink: function(e) {
+        if (e.target.name === 'linkButton') {
+            this._open2gis = this._getOpenUrl();
+            e.target.href = this._open2gis;
+        }
     },
 
     _update: function(lang, osm, countryCode) {
         if (!this._map) { return; }
-
-        this._open2gis = this._getOpenUrl();
 
         if (typeof osm !== 'undefined') {
             this._osm = osm;
@@ -119,17 +163,21 @@ DG.Control.Attribution.include({
         this._container.innerHTML = copyright + prefixAndAttribs.join(' | ');
 
     },
+
+
     _getOpenUrl: function() {
-        if (!this._map.projectDetector) {
-            return '';
+        if (this._markerToRoute) {
+            return DG.Util.template(DG.config.ppLink2gis, {
+                'gislink': this._getLink('open_link'),
+                'center': this._map.getCenter().lng + ',' + this._map.getCenter().lat,
+                'zoom': this._map.getZoom(),
+                'rsType': this._map.getZoom() > 11 ? 'bus' : 'car',
+                'point': this._markerToRoute._latlng.lng + ',' + this._markerToRoute._latlng.lat
+            });
         }
-        var project = this._map.projectDetector.getProject();
-        if (!project) {
-            return '';
-        }
-        return DG.Util.template(DG.config.openLink, {
-            'domain': project.domain,
-            'projectCode': project.code,
+
+        return DG.Util.template(DG.config.openLink2gis, {
+            'gislink': this._getLink('open_link'),
             'center': this._map.getCenter().lng + ',' + this._map.getCenter().lat,
             'zoom': this._map.getZoom(),
         });
@@ -139,11 +187,15 @@ DG.Control.Attribution.include({
         var btn =
         {
             name: 'open',
-            label: this.t('open_on'),
+            label: this._markerToRoute ? this.t('route_on') : this.t('open_on'),
         }
+
+        // Do not show link button if button don't have translate to current language or map options logotype set true
+        var isHideButton = btn.label == 'open_on' || btn.label == 'route_on' || this._logotype;
+
         return {
             'osm': this._osm,
-            'logotype': this.t('open_on') == 'open_on' || this._logotype,
+            'logotype': isHideButton,
             'work_on': this.t('work_on'),
             'work_on_with_osm': this.t('work_on_with_osm'),
             'lang': lang,
@@ -151,6 +203,7 @@ DG.Control.Attribution.include({
             'copyright_license': this._getLink('copyright_license'),
             'copyright_logo': this._getLink('copyright_logo'),
             'open2gis_link': this._open2gis,
+            'open2gis_name': 'linkButton',
             'license_agreement': this.t('license_agreement'),
             'dir': lang !== 'ar' ? 'ltr' : 'rtl',
             'btn': btn
