6ec1be460f66015fbc8d411c49cd038fe62b0495
o.kalyanov <o.kalyanov@flamp.ru>
2024-04-02 17:54:04 +0100

    Release 3.7.4

--- app/loader.js
+++ app/loader.js
@@ -3,7 +3,7 @@
 
     var isJSRequested = false;
     var rejects = [];
-    var version = 'v3.7.3';
+    var version = 'v3.7.4';
     var baseURL = '__BASE_URL__';
     var query = __QUERY__;
     var isLazy = query.lazy === 'true';
