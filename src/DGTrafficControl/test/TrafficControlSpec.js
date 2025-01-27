b82380c680554fd759ef2003f75ff9377dd0b2ad
Vladimir Airikh <vladimir.airich@gmail.com>
2025-01-27 13:44:15 +0300

    TILES-7179 Removed Google Analytics, fixed CI, fixed no project test case (#576)
    * TILES-7179 Removed Google Analytics
    
    * Fixed CI, skipped failed test
    
    * Unskip fixed test
    
    * Removed the unused config property
    
    ---------
    
    Co-authored-by: Vladimir-Ayrikh <v.ayrikh@2gis.ru>

--- src/DGTrafficControl/test/TrafficControlSpec.js
+++ src/DGTrafficControl/test/TrafficControlSpec.js
@@ -5,7 +5,7 @@ describe('DG.TrafficControl', function() {
 
         centerNsk = [55.017493, 82.819576],
         centerSpb = [59.937706, 30.13249],
-        centerWithoutProject = [55.363990665081126, 77.81307220458986];
+        centerWithoutProject = [77.81307220458986, 55.363990665081126]; // севернее Новой земли
 
     after(function () {
         controlClass = controlParentActiveClass = controlParentHiddenClass = centerNsk = null;
@@ -25,61 +25,61 @@ describe('DG.TrafficControl', function() {
                 center: centerSpb,
                 zoom: 15
             });
-    
+
             control = mapContainer.getElementsByClassName(controlClass);
             trafficControl = DG.control.traffic();
-    
+
             document.body.appendChild(mapContainer);
             map.addControl(trafficControl);
         });
-    
+
         after(function () {
             map.remove();
             document.body.removeChild(mapContainer);
             mapContainer = map = control = trafficControl = controlParent = null;
         });
-    
+
         describe('module init', function () {
             it('traffic control show', function () {
                 expect(control.length).to.be(1);
-    
+
                 control = control[0];
                 controlParent = control.parentElement;
             });
-    
+
             it('remove hide class from control parent element', function () {
                 expect(controlParent.className).not.contain(controlParentHiddenClass);
             });
         });
-    
+
         describe('turn on control', function () {
             before(function () {
                 happen.click(controlParent);
             });
-    
+
             it('add active class to control parent element', function () {
                 expect(controlParent.className).to.contain(controlParentActiveClass);
             });
-    
+
             it('add traffic layer to map', function () {
                 expect(map.hasLayer(trafficControl._trafficLayer)).to.be(true);
             });
         });
-    
+
         describe('turn off control', function () {
             before(function () {
                 happen.click(controlParent);
             });
-    
+
             it('remove active class from control parent element', function () {
                 expect(controlParent.className).not.contain(controlParentActiveClass);
             });
-    
+
             it('remove traffic layer from map', function () {
                 expect(map.hasLayer(trafficControl._trafficLayer)).to.be(false);
             });
         });
-    
+
         describe('traffic point request', function () {
             var xhr, center, zoom, requests;
 
@@ -88,13 +88,13 @@ describe('DG.TrafficControl', function() {
                 center = map.getCenter();
                 xhr = sinon.useFakeXMLHttpRequest();
                 requests =  [];
-    
+
                 xhr.onCreate = function (xhr) {
                     requests.push(xhr);
                 };
-    
+
                 happen.click(controlParent);
-    
+
             });
 
             after(function () {
@@ -102,13 +102,13 @@ describe('DG.TrafficControl', function() {
                 map.setView(center, zoom);
                 happen.click(controlParent);
             });
-    
+
             it('traffic points request called', function () {
                 var urlRE = new RegExp('http://traffic\\d+.maps.2gis.com/\\w+/meta/score/0/');
                 expect(urlRE.test(requests[0].url)).to.be(true);
             });
         });
-    
+
         describe('traffic point timer request', function () {
             var xhr, center, zoom, requests, clock;
 
@@ -118,13 +118,13 @@ describe('DG.TrafficControl', function() {
                 xhr = sinon.useFakeXMLHttpRequest();
                 clock = sinon.useFakeTimers();
                 requests =  [];
-    
+
                 xhr.onCreate = function (xhr) {
                     requests.push(xhr);
                 };
-    
+
                 happen.click(controlParent);
-    
+
             });
 
             after(function () {
@@ -133,7 +133,7 @@ describe('DG.TrafficControl', function() {
                 map.setView(center, zoom);
                 happen.click(controlParent);
             });
-    
+
             it('traffic points update request called', function () {
                 var urlRE = new RegExp('http://traffic\\d+.maps.2gis.com/\\w+/meta/speed/time/');
                 clock.tick(DG.config.trafficLayerUpdateInterval + 1);
@@ -155,16 +155,16 @@ describe('DG.TrafficControl', function() {
                 center: centerSpb,
                 zoom: 15
             });
-    
+
             trafficControl = DG.control.traffic();
-    
+
             document.body.appendChild(mapContainer);
             map.addControl(trafficControl);
 
             control = mapContainer.getElementsByClassName(controlClass)[0];
             controlParent = control.parentElement;
         });
-    
+
         afterEach(function () {
             map.remove();
             document.body.removeChild(mapContainer);
@@ -234,7 +234,7 @@ describe('DG.TrafficControl', function() {
                     expect(map.hasLayer(trafficControl._trafficLayer)).to.be(true);
                     done();
                 };
-                
+
                 map.setView(centerWithoutProject, 15, {animate: false});
                 map.on('projectchange', onProjectChange);
                 map.setView(centerSpb, 15, {animate: false});
@@ -246,28 +246,28 @@ describe('DG.TrafficControl', function() {
             });
         });
 
-        describe('traffic test on project change', function () {    
+        describe('traffic test on project change', function () {
             describe('with traffic off', function () {
                     // traffic layer is off after map initialization
-    
+
                 it('control parent element haven\'t active class', function () {
                     map.setView(centerNsk, 15, {animate: false});
                     expect(controlParent.className).not.contain(controlParentActiveClass);
                 });
             });
-    
-            describe('with traffic on', function () {                    
+
+            describe('with traffic on', function () {
                 beforeEach(function () {
                     // traffic layer is on, click on traffic control
                     happen.click(controlParent);
                     control.innerHTML = -50;
                 });
-    
+
                 it('control parent element have active class', function () {
                     map.setView(centerNsk, 15, {animate: false});
                     expect(controlParent.className).to.contain(controlParentActiveClass);
                 });
-    
+
                 it('update traffic balls in control', function () {
                     expect(control.innerHTML).not.be(-50);
                 });
