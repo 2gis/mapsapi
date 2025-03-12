b49bd5a9db0f0cd013c85d916cc7bdc49fe04cb1
nortren <nortren@inbox.ru>
2025-03-12 12:33:51 +0300

    add a few text lng

--- src/DGAttribution/lang/ar.js
+++ src/DGAttribution/lang/ar.js
@@ -1,6 +1,7 @@
 DG.Control.Attribution.Dictionary.ar = {
     license_agreement: 'إتفاقية ترخيص',
     work_on: 'يعمل بتطبيق 2GIS',
+    has_no_key: 'مفتاح مطلوب للعمل بشكل صحيح. يرجى التواصل مع api@2gis.com',
     open_on: 'Open in 2GIS',
     route_on: 'Route on 2GIS'
 };
--- src/DGAttribution/lang/it.js
+++ src/DGAttribution/lang/it.js
@@ -1,4 +1,5 @@
 DG.Control.Attribution.Dictionary.it = {
     license_agreement : 'Accordo di licenza',
+    has_no_key: 'È necessaria una chiave per il corretto funzionamento. Si prega di contattare api@2gis.com',
     work_on: '2GIS API'
 };
