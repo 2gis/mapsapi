dae2d5b64c01558460ca2bfb47b17a4cfa05935b
nortren <nortren@inbox.ru>
2025-03-19 16:31:58 +0300

    add git secrets

--- src/DGEntrance/test/EntranceSpec.js
+++ src/DGEntrance/test/EntranceSpec.js
@@ -16,6 +16,7 @@ describe('DG.Entrance', function () {
         map = new DG.Map(mapContainer, {
             center: new DG.LatLng(69.349552990994837, 87.75222519148015),
             zoom: 17,
+            key: window.__karma__.config.secretKey,
         });
         entrance = new DG.Entrance({
             'is_primary': false,
