fb15769521bf1058a321b57933c7a0ca9e3330b9
nortren <nortren@inbox.ru>
2025-05-28 11:32:37 +0300

    TILES-7572 add show comp POI logic

--- src/DGAttribution/lang/en.js
+++ src/DGAttribution/lang/en.js
@@ -1,5 +1,5 @@
 DG.Control.Attribution.Dictionary.en = {
     license_agreement: 'License agreement',
-    has_no_key: 'ReactJS API by 2GIS will become paid starting July 1. To avoid service interruption, request a key from your manager or email api@2gis.ru',
+    has_no_key: 'RasterJS API by 2GIS will become paid starting July 1. To avoid service interruption, request a key from your manager or email api@2gis.ru',
     work_on: 'Uses 2GIS API'
 };
