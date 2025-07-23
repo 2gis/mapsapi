18864548b3d0bbbfe8bdca8fe8b11faaf6f59a03
Michel Beloshitsky <itano@yandex.ru>
2025-07-23 17:21:55 +0300

    TILES-7820: Более мягкое сообщение о нужности ключа (#600)
    * fix message
    
    * rm quote

--- src/DGAttribution/lang/ru.js
+++ src/DGAttribution/lang/ru.js
@@ -1,7 +1,7 @@
 DG.Control.Attribution.Dictionary.ru = {
     license_agreement: 'Лицензионное соглашение',
     work_on: 'Работает на API 2ГИС',
-    has_no_key: 'С 1 июля RasterJS API станет платным. Для работы нужен ключ. Помощь: api@2gis.ru',
+    has_no_key: 'Для корректной работы Raster JS API нужен ключ. Помощь: api@2gis.ru',
     open_on: 'Открыть в 2ГИС',
     route_on: 'Доехать с 2ГИС'
 };
